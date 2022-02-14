f1b5dd49471d36478ddb66707d02c0bf39086d33
Thuyen Truong <thuyen@bitmark.com>
2022-02-15 02:33:26 +0700

    [db] personaDB

--- lib/database/app_database.g.dart
+++ lib/database/app_database.g.dart
@@ -270,3 +270,156 @@ class _$AssetTokenDao extends AssetTokenDao {
     await _assetTokenDeletionAdapter.delete(asset);
   }
 }
+
+class $FloorCloudDatabase {
+  /// Creates a database builder for a persistent database.
+  /// Once a database is built, you should keep a reference to it and re-use it.
+  static _$CloudDatabaseBuilder databaseBuilder(String name) =>
+      _$CloudDatabaseBuilder(name);
+
+  /// Creates a database builder for an in memory database.
+  /// Information stored in an in memory database disappears when the process is killed.
+  /// Once a database is built, you should keep a reference to it and re-use it.
+  static _$CloudDatabaseBuilder inMemoryDatabaseBuilder() =>
+      _$CloudDatabaseBuilder(null);
+}
+
+class _$CloudDatabaseBuilder {
+  _$CloudDatabaseBuilder(this.name);
+
+  final String? name;
+
+  final List<Migration> _migrations = [];
+
+  Callback? _callback;
+
+  /// Adds migrations to the builder.
+  _$CloudDatabaseBuilder addMigrations(List<Migration> migrations) {
+    _migrations.addAll(migrations);
+    return this;
+  }
+
+  /// Adds a database [Callback] to the builder.
+  _$CloudDatabaseBuilder addCallback(Callback callback) {
+    _callback = callback;
+    return this;
+  }
+
+  /// Creates the database and initializes it.
+  Future<CloudDatabase> build() async {
+    final path = name != null
+        ? await sqfliteDatabaseFactory.getDatabasePath(name!)
+        : ':memory:';
+    final database = _$CloudDatabase();
+    database.database = await database.open(
+      path,
+      _migrations,
+      _callback,
+    );
+    return database;
+  }
+}
+
+class _$CloudDatabase extends CloudDatabase {
+  _$CloudDatabase([StreamController<String>? listener]) {
+    changeListener = listener ?? StreamController<String>.broadcast();
+  }
+
+  PersonaDao? _personaDaoInstance;
+
+  Future<sqflite.Database> open(String path, List<Migration> migrations,
+      [Callback? callback]) async {
+    final databaseOptions = sqflite.OpenDatabaseOptions(
+      version: 1,
+      onConfigure: (database) async {
+        await database.execute('PRAGMA foreign_keys = ON');
+        await callback?.onConfigure?.call(database);
+      },
+      onOpen: (database) async {
+        await callback?.onOpen?.call(database);
+      },
+      onUpgrade: (database, startVersion, endVersion) async {
+        await MigrationAdapter.runMigrations(
+            database, startVersion, endVersion, migrations);
+
+        await callback?.onUpgrade?.call(database, startVersion, endVersion);
+      },
+      onCreate: (database, version) async {
+        await database.execute(
+            'CREATE TABLE IF NOT EXISTS `Persona` (`uuid` TEXT NOT NULL, `name` TEXT NOT NULL, `createdAt` INTEGER NOT NULL, PRIMARY KEY (`uuid`))');
+
+        await callback?.onCreate?.call(database, version);
+      },
+    );
+    return sqfliteDatabaseFactory.openDatabase(path, options: databaseOptions);
+  }
+
+  @override
+  PersonaDao get personaDao {
+    return _personaDaoInstance ??= _$PersonaDao(database, changeListener);
+  }
+}
+
+class _$PersonaDao extends PersonaDao {
+  _$PersonaDao(this.database, this.changeListener)
+      : _queryAdapter = QueryAdapter(database),
+        _personaInsertionAdapter = InsertionAdapter(
+            database,
+            'Persona',
+            (Persona item) => <String, Object?>{
+                  'uuid': item.uuid,
+                  'name': item.name,
+                  'createdAt': _dateTimeConverter.encode(item.createdAt)
+                }),
+        _personaUpdateAdapter = UpdateAdapter(
+            database,
+            'Persona',
+            ['uuid'],
+            (Persona item) => <String, Object?>{
+                  'uuid': item.uuid,
+                  'name': item.name,
+                  'createdAt': _dateTimeConverter.encode(item.createdAt)
+                });
+
+  final sqflite.DatabaseExecutor database;
+
+  final StreamController<String> changeListener;
+
+  final QueryAdapter _queryAdapter;
+
+  final InsertionAdapter<Persona> _personaInsertionAdapter;
+
+  final UpdateAdapter<Persona> _personaUpdateAdapter;
+
+  @override
+  Future<List<Persona>> getAllPersona() async {
+    return _queryAdapter.queryList('SELECT * FROM Persona',
+        mapper: (Map<String, Object?> row) => Persona(
+            uuid: row['uuid'] as String,
+            name: row['name'] as String,
+            createdAt: _dateTimeConverter.decode(row['createdAt'] as int)));
+  }
+
+  @override
+  Future<Persona?> findById(String uuid) async {
+    return _queryAdapter.query('SELECT * FROM Persona WHERE uuid = ?1',
+        mapper: (Map<String, Object?> row) => Persona(
+            uuid: row['uuid'] as String,
+            name: row['name'] as String,
+            createdAt: _dateTimeConverter.decode(row['createdAt'] as int)),
+        arguments: [uuid]);
+  }
+
+  @override
+  Future<void> insertPersona(Persona persona) async {
+    await _personaInsertionAdapter.insert(persona, OnConflictStrategy.abort);
+  }
+
+  @override
+  Future<void> updatePersona(Persona persona) async {
+    await _personaUpdateAdapter.update(persona, OnConflictStrategy.abort);
+  }
+}
+
+// ignore_for_file: unused_element
+final _dateTimeConverter = DateTimeConverter();
