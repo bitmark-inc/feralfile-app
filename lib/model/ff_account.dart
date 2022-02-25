af9ac05b506774aa0d7e6c79d895b67a1cbab760
Thuyen Truong <thuyen@bitmark.com>
2022-02-25 13:31:43 +0700

    [f] migration iOS data

--- lib/model/ff_account.dart
+++ lib/model/ff_account.dart
@@ -7,20 +7,14 @@ part 'ff_account.g.dart';
 @JsonSerializable()
 class FFAccount {
   String accountNumber;
-  String email;
   String alias;
   String location;
-  String website;
-  String avatarURI;
   WyreWallet? wyreWallet;
 
   FFAccount(
       {required this.accountNumber,
-      required this.email,
       required this.alias,
       required this.location,
-      required this.website,
-      required this.avatarURI,
       required this.wyreWallet});
 
   factory FFAccount.fromJson(Map<String, dynamic> json) =>
