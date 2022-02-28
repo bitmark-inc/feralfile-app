6fdfa6fbb28e0512fb4d431db727434721635d79
hvthhien <hvthhien@gmail.com>
2022-02-28 17:37:15 +0700

    update domain to autonomy.io

--- lib/common/app_config.dart
+++ lib/common/app_config.dart
@@ -6,14 +6,14 @@ class AppConfig {
       "https://rinkeby.infura.io/v3/20aba74f4e8642b88808ff4df18c10ff",
       "https://hangzhounet.smartpy.io",
       "https://api.test.bitmark.com",
-      "https://nft-indexer.test.bitmark.com",
+      "https://indexer.test.autonomy.io",
       "https://autonomy-auth.test.bitmark.com",
       "https://feralfile1.dev.bitmark.com");
   static const AppConfigNetwork mainNetworkConfig = AppConfigNetwork(
       "https://mainnet.infura.io/v3/ab0e0fe9710b412ca73bb044713a3523",
       "https://mainnet.smartpy.io/",
       "https://api.bitmark.com",
-      "https://nft-indexer.bitmark.com",
+      "https://indexer.autonomy.io",
       "https://autonomy-auth.test.bitmark.com",
       "https://feralfile.com");
 }
