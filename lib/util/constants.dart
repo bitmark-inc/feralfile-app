c9b83f8d178e86b2f6a516ea46fed395b729ab6c
Anh Nguyen <anhnguyen@bitmark.com>
2022-03-23 16:03:54 +0700

    use the image from cloudflare as thumbnails

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,6 +1,8 @@
 import 'package:package_info_plus/package_info_plus.dart';
 
 const INDEXER_TOKENS_MAXIMUM = 50;
+const CLOUDFLAREIMAGEURLPREFIX =
+    'https://imagedelivery.net/iCRs13uicXIPOWrnuHbaKA/';
 
 Future<bool> isAppCenterBuild() async {
   final PackageInfo info = await PackageInfo.fromPlatform();
