5197db7a4597fbfeb4d8687788e80422f0baba0b
hvthhien <hvthhien@gmail.com>
2022-03-29 17:02:51 +0700

    refactor code

--- lib/screen/cloud/cloud_android_page.dart
+++ lib/screen/cloud/cloud_android_page.dart
@@ -10,19 +10,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:open_settings/open_settings.dart';
 
-class CloudErrorPage extends StatefulWidget {
-  final bool isEncryptionError;
+class CloudAndroidPage extends StatefulWidget {
+  final bool? isEncryptionAvailable;
 
-  const CloudErrorPage({Key? key, required this.isEncryptionError})
+  const CloudAndroidPage({Key? key, required this.isEncryptionAvailable})
       : super(key: key);
 
   @override
-  State<CloudErrorPage> createState() => _CloudErrorPageState();
+  State<CloudAndroidPage> createState() => _CloudAndroidPageState();
 }
 
-class _CloudErrorPageState extends State<CloudErrorPage> with WidgetsBindingObserver {
+class _CloudAndroidPageState extends State<CloudAndroidPage> with WidgetsBindingObserver {
 
-  bool isEncryptionError = false;
+  bool? isEncryptionAvailable;
 
   @override
   void initState() {
@@ -30,7 +30,7 @@ class _CloudErrorPageState extends State<CloudErrorPage> with WidgetsBindingObse
     WidgetsBinding.instance?.addObserver(this);
 
     setState(() {
-      isEncryptionError = widget.isEncryptionError;
+      isEncryptionAvailable = widget.isEncryptionAvailable;
     });
   }
 
@@ -50,26 +50,20 @@ class _CloudErrorPageState extends State<CloudErrorPage> with WidgetsBindingObse
   }
 
   Future checkCloudBackup() async {
+    if (isEncryptionAvailable == true) return;
+
     final accountService = injector<AccountService>();
     final isAndroidEndToEndEncryptionAvailable = await accountService.isAndroidEndToEndEncryptionAvailable();
 
-    if (!widget.isEncryptionError) {
-      if (isAndroidEndToEndEncryptionAvailable != null) {
-        await accountService.androidBackupKeys();
-      }
+    if (isEncryptionAvailable == isAndroidEndToEndEncryptionAvailable) return;
 
-      if (isAndroidEndToEndEncryptionAvailable == true) {
-        _continue(context);
-      } else {
-        setState(() {
-          isEncryptionError = false;
-        });
-      }
-    } else {
-      if (isAndroidEndToEndEncryptionAvailable == true) {
-        _continue(context);
-      }
+    if (isEncryptionAvailable == null && isAndroidEndToEndEncryptionAvailable != null) {
+      await accountService.androidBackupKeys();
     }
+
+    setState(() {
+      isEncryptionAvailable = isAndroidEndToEndEncryptionAvailable;
+    });
 }
 
   @override
@@ -101,7 +95,9 @@ class _CloudErrorPageState extends State<CloudErrorPage> with WidgetsBindingObse
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      isEncryptionError
+                      isEncryptionAvailable == true
+                          ? "Backed up"
+                          : isEncryptionAvailable == false
                           ? "Enable backup encryption "
                           : "Google Drive backup unavailable",
                       style: appTextTheme.headline1,
@@ -111,16 +107,23 @@ class _CloudErrorPageState extends State<CloudErrorPage> with WidgetsBindingObse
                       "Autonomy will automatically back up all of your account information securely, including cryptographic material from accounts you manage as well as links to your accounts. If you ever lose your phone, you will be able to recover everything.",
                       style: appTextTheme.bodyText1,
                     ),
-                    SizedBox(height: 16),
-                    Text(
-                      isEncryptionError
-                          ? "Automatic Google Drive backups are enabled, but you are not using end-to-end encryption. We recommend enabling it so we can securely back up your account."
-                          : "Google Drive is currently turned off on your device. If your device supports it, we recommend you enable it so we can safely back up your account. ",
-                      style: appTextTheme.headline4,
-                    ),
-                    SizedBox(height: 40),
-                    Center(
-                        child: SvgPicture.asset("assets/images/cloudOff.svg")),
+                    if (isEncryptionAvailable == true) ...[
+                      SizedBox(height: 40),
+                      Center(
+                          child: SvgPicture.asset(
+                              "assets/images/cloudOn.svg")),
+                    ] else ...[
+                      SizedBox(height: 16),
+                      Text(
+                        isEncryptionAvailable == false
+                            ? "Automatic Google Drive backups are enabled, but you are not using end-to-end encryption. We recommend enabling it so we can securely back up your account."
+                            : "Google Drive is currently turned off on your device. If your device supports it, we recommend you enable it so we can safely back up your account. ",
+                        style: appTextTheme.headline4,
+                      ),
+                      SizedBox(height: 40),
+                      Center(
+                          child: SvgPicture.asset(isEncryptionAvailable == false ? "assets/images/cloudEncryption.svg" : "assets/images/cloudOff.svg")),
+                    ],
                   ]),
             ),
           ),
@@ -131,26 +134,39 @@ class _CloudErrorPageState extends State<CloudErrorPage> with WidgetsBindingObse
   }
 
   Widget _buttonsGroup(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Expanded(
-              child: AuFilledButton(
-                text: "OPEN DEVICE SETTINGS".toUpperCase(),
-                onPress: () => isEncryptionError
-                    ? OpenSettings.openBiometricEnrollSetting()
-                    : OpenSettings.openAddAccountSetting(),
-              ),
+    if (isEncryptionAvailable == true) {
+      return Row(
+        children: [
+          Expanded(
+            child: AuFilledButton(
+              text: "CONTINUE".toUpperCase(),
+              onPress: () => _continue(context),
             ),
-          ],
-        ),
-        TextButton(
-            onPressed: () => _continue(context),
-            child: Text("CONTINUE WITHOUT IT",
-                style: appTextTheme.button?.copyWith(color: Colors.black))),
-      ],
-    );
+          ),
+        ],
+      );
+    } else {
+      return Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: AuFilledButton(
+                  text: "OPEN DEVICE SETTINGS".toUpperCase(),
+                  onPress: () => isEncryptionAvailable == false
+                      ? OpenSettings.openBiometricEnrollSetting()
+                      : OpenSettings.openAddAccountSetting(),
+                ),
+              ),
+            ],
+          ),
+          TextButton(
+              onPressed: () => _continue(context),
+              child: Text("CONTINUE WITHOUT IT",
+                  style: appTextTheme.button?.copyWith(color: Colors.black))),
+        ],
+      );
+    }
   }
 
   void _continue(BuildContext context) {
