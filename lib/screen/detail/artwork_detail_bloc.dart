a67b13c31ff59c1de6876b5930c927046987cc5f
Thuyen Truong <thuyen@bitmark.com>
2022-05-13 14:41:00 +0700

    skip calling asset-prices for not bitmark nft

--- lib/screen/detail/artwork_detail_bloc.dart
+++ lib/screen/detail/artwork_detail_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:autonomy_flutter/database/dao/asset_token_dao.dart';
 import 'package:autonomy_flutter/database/dao/provenance_dao.dart';
+import 'package:autonomy_flutter/model/asset_price.dart';
 import 'package:autonomy_flutter/screen/detail/artwork_detail_state.dart';
 import 'package:autonomy_flutter/service/feralfile_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,8 +20,12 @@ class ArtworkDetailBloc extends Bloc<ArtworkDetailEvent, ArtworkDetailState> {
 
       emit(ArtworkDetailState(asset: asset, provenances: []));
 
-      final assetPrices = await _feralFileService
-          .getAssetPrices([event.id.replaceAll("bmk--", "")]);
+      List<AssetPrice> assetPrices = [];
+
+      if (event.id.startsWith('bmk--')) {
+        assetPrices = await _feralFileService
+            .getAssetPrices([event.id.replaceAll("bmk--", "")]);
+      }
 
       emit(ArtworkDetailState(
           asset: asset,
