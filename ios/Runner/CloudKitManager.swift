0e0f9163b5f2327600a278acba8c1d5ce66e075e
Thuyen Truong <thuyen@bitmark.com>
2022-05-25 17:56:41 +0700

    [c] add LICENSE into file headers

--- ios/Runner/CloudKitManager.swift
+++ ios/Runner/CloudKitManager.swift
@@ -1,4 +1,11 @@
 //
+//  SPDX-License-Identifier: BSD-2-Clause-Patent
+//  Copyright © 2022 Bitmark. All rights reserved.
+//  Use of this source code is governed by the BSD-2-Clause Plus Patent License
+//  that can be found in the LICENSE file.
+//
+
+//
 //  CloudKitManager.swift
 //  Runner
 //
