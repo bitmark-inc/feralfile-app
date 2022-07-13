9c862fca73e87d2ee9b05993e6dcc734379b443f
hvthhien <hvthhien@gmail.com>
2022-07-13 16:34:19 +0700

    remove unused var

--- lib/util/error_handler.dart
+++ lib/util/error_handler.dart
@@ -95,11 +95,6 @@ Future showErrorDialog(BuildContext context, String title, String description,
   isShowErrorDialogWorking = DateTime.now();
   final theme = AuThemeManager.get(AppTheme.sheetTheme);
 
-  var cuttedColor = Color(0xFF737373);
-  if (ModalRoute.of(context)?.settings.name == AppRouter.scanQRPage) {
-    cuttedColor = Color.fromARGB(255, 62, 60, 61);
-  }
-
   Vibrate.feedback(FeedbackType.warning);
   await showModalBottomSheet(
       context: context,
