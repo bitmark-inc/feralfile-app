189ec64ece159407581722213b5b2be133b1a6c8
Đặng Quang Hưng <hungdq58@gmail.com>
2022-08-23 14:04:25 +0700

    Update subscription loading UI

--- lib/screen/settings/subscription/upgrade_box_view.dart
+++ lib/screen/settings/subscription/upgrade_box_view.dart
@@ -8,7 +8,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UpgradeBoxView {
-  static Widget getMoreAutonomyWidget(ThemeData theme, PremiumFeature feature) {
+  static Widget getMoreAutonomyWidget(ThemeData theme, PremiumFeature feature,
+      {bool autoClose = true}) {
     return Column(
       children: [
         Row(
@@ -20,7 +21,10 @@ class UpgradeBoxView {
             ),
             BlocProvider.value(
               value: UpgradesBloc(injector(), injector()),
-              child: _SubscribeView(feature: feature),
+              child: _SubscribeView(
+                feature: feature,
+                closeOnSubscribe: autoClose,
+              ),
             ),
           ],
         ),
@@ -37,8 +41,11 @@ class UpgradeBoxView {
 
 class _SubscribeView extends StatefulWidget {
   final PremiumFeature? feature;
+  final bool closeOnSubscribe;
 
-  const _SubscribeView({Key? key, required this.feature}) : super(key: key);
+  const _SubscribeView(
+      {Key? key, required this.feature, this.closeOnSubscribe = true})
+      : super(key: key);
 
   @override
   State<_SubscribeView> createState() => _SubscribeViewState();
@@ -61,7 +68,9 @@ class _SubscribeViewState extends State<_SubscribeView> {
           onPressed: () => UpgradesView.showSubscriptionDialog(
               context, state.productDetails?.price, widget.feature, (() {
             context.read<UpgradesBloc>().add(UpgradePurchaseEvent());
-            Navigator.of(context).pop();
+            if (widget.closeOnSubscribe) {
+              Navigator.of(context).pop();
+            }
           })),
           child: Text(
             "subscribe".tr(),
