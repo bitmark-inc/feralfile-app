7deb720c594858058e1587cd2503a6a17700a328
Long Vu <long@bitmark.com>
2022-08-26 12:00:21 +0700

    Change device to run auto

--- integration_test/external_tests/test_data/test_configurations.dart
+++ integration_test/external_tests/test_data/test_configurations.dart
@@ -10,8 +10,8 @@ const CHROME_MOBILE_BROWSER_PROFILE = {
 };
 Map<String, String> AUTONOMY_PROFILE(String currentDir) => {
       'platformName': "Android",
-      'platformVersion': "11",
-      'deviceName': 'Redmi Note 11S',
+      'platformVersion': "12",
+      'deviceName': 'Galaxy A52s 5G',
       'app':
           "${currentDir}/build/app/outputs/flutter-apk/app-inhouse-release.apk",
       'appPackage': "com.bitmark.autonomy_client.inhouse",
