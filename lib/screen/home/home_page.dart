2aa17acfa8495ab941d6d68e74a339072e8cf0e9
phuocbitmark <phuoc@bitmark.com>
2022-09-20 15:23:01 +0700

    fix sync tv for iphone
    Signed-off-by: phuocbitmark <phuoc@bitmark.com>

--- lib/screen/home/home_page.dart
+++ lib/screen/home/home_page.dart
@@ -234,8 +234,7 @@ class _HomePageState extends State<HomePage>
             injector<NavigationService>().navigateTo(AppRouter.tvConnectPage,
                 arguments: WCConnectPageArgs(id!, peerMeta!));
             config.deleteTVConnectData();
-          }
-          if (state.status != IAPProductStatus.loading &&
+          } else if (state.status != IAPProductStatus.loading &&
               state.status != IAPProductStatus.pending) {
             injector<WalletConnectService>().rejectRequest(peerMeta!, id!);
             config.deleteTVConnectData();
