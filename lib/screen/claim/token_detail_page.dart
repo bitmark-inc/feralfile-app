abc0deaa1ca00f50660907a3af0da0096d9727c1
hvthhien <hvthhien@gmail.com>
2022-11-30 09:42:02 +0700

    support html for description

--- lib/screen/claim/token_detail_page.dart
+++ lib/screen/claim/token_detail_page.dart
@@ -14,6 +14,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 class TokenDetailPage extends StatefulWidget {
   final FFArtwork artwork;
@@ -71,9 +72,9 @@ class _TokenDetailPageState extends State<TokenDetailPage> {
               const SizedBox(height: 24.0),
               Padding(
                 padding: ResponsiveLayout.getPadding,
-                child: Text(
+                child: HtmlWidget(
                   artwork.description,
-                  style: theme.primaryTextTheme.bodyText1,
+                  textStyle: theme.primaryTextTheme.bodyText1,
                 ),
               ),
               const SizedBox(height: 40.0),
