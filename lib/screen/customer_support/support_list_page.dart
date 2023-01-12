7e72cffb5d6840fed52e702cbf9a520282f8c21e
phuoc <phuoc@bitmark.com>
2023-01-12 09:27:34 +0700

    remove unused import
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/screen/customer_support/support_list_page.dart
+++ lib/screen/customer_support/support_list_page.dart
@@ -19,7 +19,6 @@ import 'package:autonomy_flutter/util/style.dart';
 import 'package:autonomy_flutter/view/back_appbar.dart';
 import 'package:autonomy_flutter/view/responsive.dart';
 import 'package:autonomy_theme/autonomy_theme.dart';
-import 'package:autonomy_theme/style/colors.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
