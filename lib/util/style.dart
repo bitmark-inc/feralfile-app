30e58443dc51e7b872c5386f35a80e2aac5b6ef9
hvthhien <hvthhien@gmail.com>
2023-01-19 09:49:46 +0700

    Improve text size Editorial

--- lib/util/style.dart
+++ lib/util/style.dart
@@ -252,7 +252,7 @@ MarkdownStyleSheet editorialMarkDownStyle(BuildContext context) {
       theme.textTheme.ppMori400White12.copyWith(fontSize: 17);
   final textStyleGreen =
       theme.textTheme.ppMori400Green12.copyWith(fontSize: 17);
-  final textStyleGrey = theme.textTheme.ppMori400Grey12..copyWith(fontSize: 17);
+  final textStyleGrey = theme.textTheme.ppMori400Grey12.copyWith(fontSize: 17);
   return MarkdownStyleSheet(
     a: const TextStyle(
       fontFamily: AppTheme.ppMori,
@@ -284,7 +284,7 @@ MarkdownStyleSheet editorialMarkDownStyle(BuildContext context) {
     del: const TextStyle(
         decoration: TextDecoration.lineThrough, color: textColor),
     blockquote: textStyleWhite,
-    img: textStyleWhite,
+    img: textStyleGrey.copyWith(fontSize: 12),
     checkbox: textStyleWhite.copyWith(color: theme.colorScheme.secondary),
     blockSpacing: 15.0,
     listIndent: 24.0,
