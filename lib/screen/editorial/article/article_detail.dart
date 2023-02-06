b67f286d0a0eeaeb79cc50caf68f58696de0f8bb
PuPha <nguyensanghso@gmail.com>
2023-02-06 11:55:01 +0700

    Add Custom Tag for Editorial

--- lib/screen/editorial/article/article_detail.dart
+++ lib/screen/editorial/article/article_detail.dart
@@ -12,13 +12,13 @@ import 'package:autonomy_flutter/service/metric_client_service.dart';
 import 'package:autonomy_flutter/util/constants.dart';
 import 'package:autonomy_flutter/util/string_ext.dart';
 import 'package:autonomy_flutter/util/style.dart';
+import 'package:autonomy_flutter/view/markdown_view.dart';
 import 'package:autonomy_flutter/view/responsive.dart';
 import 'package:autonomy_theme/autonomy_theme.dart';
 import 'package:dio/dio.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
@@ -85,103 +85,92 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
                   ),
                   const SizedBox(height: 32.0),
                   FutureBuilder<Response<String>>(
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData &&
-                          snapshot.data?.statusCode == 200) {
-                        return Column(
-                          children: [
-                            Markdown(
-                              key: const Key("githubMarkdown"),
-                              data: snapshot.data!.data!,
-                              softLineBreak: true,
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              padding: const EdgeInsets.only(bottom: 50),
-                              styleSheet: editorialMarkDownStyle(context),
-                              onTapLink: (text, href, title) async {
-                                if (href == null) return;
-                                if (await canLaunchUrlString(href)) {
-                                  launchUrlString(href,
-                                      mode: LaunchMode.externalApplication);
-                                  metricClient.addEvent(
-                                      MixpanelEvent.tabOnLinkInEditorial,
-                                      data: {
-                                        'link': href,
-                                        'name': title,
-                                      });
-                                }
-                              },
-                            ),
-                            if (widget.post.reference != null)
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  border:
-                                      Border.all(color: AppColor.auSuperTeal),
-                                ),
-                                padding: const EdgeInsets.all(15.0),
-                                child: Column(
-                                  children: [
-                                    if (widget.post.tag == "Essay") ...[
-                                      PublisherView(
-                                          publisher: widget.post.publisher),
-                                      const SizedBox(height: 10.0),
-                                      Text(
-                                        widget.post.publisher.intro ?? "",
-                                        style: theme.textTheme.ppMori400White12,
-                                      ),
-                                      const SizedBox(height: 32.0),
-                                    ],
-                                    _referenceRow(
-                                      context,
-                                      name: "location".tr(),
-                                      value:
-                                          widget.post.reference?.location ?? "",
-                                    ),
-                                    const Divider(
-                                        height: 20, color: AppColor.greyMedium),
-                                    _referenceRowWithLinks(
-                                      context,
-                                      name: "web".tr(),
-                                      links: [
-                                        Pair(
-                                            widget.post.reference?.website ??
-                                                "",
-                                            widget.post.reference?.website
-                                                    .toUrl() ??
-                                                "")
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData &&
+                            snapshot.data?.statusCode == 200) {
+                          return Column(
+                            children: [
+                              AuMarkdown(
+                                  data: snapshot.data!.data!,
+                                  styleSheet: editorialMarkDownStyle(context)),
+                              const SizedBox(height: 50),
+                              if (widget.post.reference != null)
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    border:
+                                        Border.all(color: AppColor.auSuperTeal),
+                                  ),
+                                  padding: const EdgeInsets.all(15.0),
+                                  child: Column(
+                                    children: [
+                                      if (widget.post.tag == "Essay") ...[
+                                        PublisherView(
+                                            publisher: widget.post.publisher),
+                                        const SizedBox(height: 10.0),
+                                        Text(
+                                          widget.post.publisher.intro ?? "",
+                                          style:
+                                              theme.textTheme.ppMori400White12,
+                                        ),
+                                        const SizedBox(height: 32.0),
                                       ],
-                                    ),
-                                    const Divider(
-                                        height: 20, color: AppColor.greyMedium),
-                                    _referenceRowWithLinks(
-                                      context,
-                                      name: "social".tr(),
-                                      links: widget.post.reference?.socials
-                                              .map((e) => Pair(e.name, e.url))
-                                              .toList() ??
-                                          [],
-                                    ),
-                                  ],
+                                      _referenceRow(
+                                        context,
+                                        name: "location".tr(),
+                                        value:
+                                            widget.post.reference?.location ??
+                                                "",
+                                      ),
+                                      const Divider(
+                                          height: 20,
+                                          color: AppColor.greyMedium),
+                                      _referenceRowWithLinks(
+                                        context,
+                                        name: "web".tr(),
+                                        links: [
+                                          Pair(
+                                              widget.post.reference?.website ??
+                                                  "",
+                                              widget.post.reference?.website
+                                                      .toUrl() ??
+                                                  "")
+                                        ],
+                                      ),
+                                      const Divider(
+                                          height: 20,
+                                          color: AppColor.greyMedium),
+                                      _referenceRowWithLinks(
+                                        context,
+                                        name: "social".tr(),
+                                        links: widget.post.reference?.socials
+                                                .map((e) => Pair(e.name, e.url))
+                                                .toList() ??
+                                            [],
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                          ],
-                        );
-                      } else if (snapshot.hasError ||
-                          (snapshot.data != null &&
-                              snapshot.data?.statusCode != 200)) {
-                        return Center(
-                            child: Text(
-                          "error_loading_content".tr(),
-                          style: theme.textTheme.ppMori400White12,
-                        ));
-                      } else {
-                        return const Center(
-                            child: CupertinoActivityIndicator());
-                      }
-                    },
-                    future: _dio.get<String>(widget.post.content["data"]),
-                  ),
+                            ],
+                          );
+                        } else if (snapshot.hasError ||
+                            (snapshot.data != null &&
+                                snapshot.data?.statusCode != 200)) {
+                          return Center(
+                              child: Text(
+                            "error_loading_content".tr(),
+                            style: theme.textTheme.ppMori400White12,
+                          ));
+                        } else {
+                          return const Center(
+                              child: CupertinoActivityIndicator());
+                        }
+                      },
+                      future: _dio.get<String>(
+                        widget.post.content["data"],
+                      )
+                      //'https://raw.githubusercontent.com/ppupha/Bitmark/main/README.md'),
+                      ),
                   const SizedBox(height: 64.0),
                 ],
               ),
