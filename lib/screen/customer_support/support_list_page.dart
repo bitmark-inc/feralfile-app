21fc5f78c4d147961e37d3d3bc91329e57766114
phuoc <phuoc@bitmark.com>
2023-02-09 11:06:17 +0700

    fix conflit
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/screen/customer_support/support_list_page.dart
+++ lib/screen/customer_support/support_list_page.dart
@@ -322,17 +322,7 @@ class _SupportListPageState extends State<SupportListPage>
     return Row(
       children: [
         const SizedBox(width: 8),
-        Padding(
-          padding: const EdgeInsets.only(top: 4),
-          child: Container(
-            decoration: const BoxDecoration(
-              color: Colors.red,
-              shape: BoxShape.circle,
-            ),
-            width: 10,
-            height: 10,
-          ),
-        )
+        Padding(padding: const EdgeInsets.only(top: 4), child: redDotIcon())
       ],
     );
   }
