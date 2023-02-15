d9e6eee337f88d37bbbc97c0aadac81f3a52fa6d
Trung <trunghuynh1999@gmail.com>
2023-02-15 15:05:53 +0700

    refactor code

--- lib/screen/app_router.dart
+++ lib/screen/app_router.dart
@@ -122,7 +122,6 @@ import 'package:autonomy_flutter/screen/wallet_connect/wc_sign_message_page.dart
 import 'package:autonomy_flutter/service/audit_service.dart';
 import 'package:autonomy_flutter/service/configuration_service.dart';
 import 'package:autonomy_flutter/util/constants.dart';
-import 'package:autonomy_flutter/util/tezos_beacon_channel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nft_collection/models/asset_token.dart';
