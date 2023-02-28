f101a6ae4324a8d8bd432c4f2220335e98ca65b0
Trung <trunghuynh1999@gmail.com>
2023-02-28 10:25:10 +0700

    update rebranding tv

--- lib/screen/wallet_connect/tv_connect_page.dart
+++ lib/screen/wallet_connect/tv_connect_page.dart
@@ -20,12 +20,10 @@ import 'package:autonomy_flutter/view/back_appbar.dart';
 import 'package:autonomy_flutter/view/primary_button.dart';
 import 'package:autonomy_flutter/view/responsive.dart';
 
-import 'package:autonomy_flutter/view/au_filled_button.dart';
 import 'package:autonomy_theme/autonomy_theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:uuid/uuid.dart';
 
 class TVConnectPage extends StatefulWidget {
