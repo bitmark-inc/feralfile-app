209ef9c6e986d8d6220e806a0825b2e6358111a8
PuPha <nguyensanghso@gmail.com>
2023-03-30 09:54:59 +0700

    Fix min size

--- lib/util/text_style_ext.dart
+++ lib/util/text_style_ext.dart
@@ -1,7 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 extension TextStyleExtension on TextStyle {
+
+  static const double _minFontSize = 8.0;
+
   TextStyle adjustSize(double size) {
-    return copyWith(fontSize: fontSize! + size);
+    return copyWith(fontSize: max(fontSize! + size, _minFontSize));
   }
 }
