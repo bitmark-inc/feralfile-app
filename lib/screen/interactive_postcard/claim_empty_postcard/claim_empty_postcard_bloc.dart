076ab213815a5f634c8345266db96c8f0d2d183b
PuPha <nguyensanghso@gmail.com>
2023-04-11 14:19:57 +0700

    Add try debug

--- lib/screen/interactive_postcard/claim_empty_postcard/claim_empty_postcard_bloc.dart
+++ lib/screen/interactive_postcard/claim_empty_postcard/claim_empty_postcard_bloc.dart
@@ -10,13 +10,14 @@ import 'package:autonomy_flutter/service/metric_client_service.dart';
 import 'package:autonomy_flutter/service/navigation_service.dart';
 import 'package:autonomy_flutter/service/postcard_service.dart';
 import 'package:autonomy_flutter/service/tezos_service.dart';
-import 'package:dio/dio.dart';
-import 'package:nft_collection/models/models.dart';
 import 'package:autonomy_flutter/util/wallet_storage_ext.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nft_collection/models/models.dart';
 import 'package:nft_collection/nft_collection.dart';
 import 'package:nft_collection/services/tokens_service.dart';
+
 import 'claim_empty_postcard_state.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ClaimEmptyPostCardBloc
     extends Bloc<ClaimEmptyPostCardEvent, ClaimEmptyPostCardState> {
@@ -98,51 +99,54 @@ class ClaimEmptyPostCardBloc
               Uint8List.fromList(utf8.encode(timestamp.toString())));
           final publicKey =
               await account.wallet.getTezosPublicKey(index: account.index);
-
-          final claimRequest = ClaimPostCardRequest(
-            address: address,
-            id: 'postcard',
-            timestamp: timestamp,
-            publicKey: publicKey,
-            signature: signature,
-          );
-          final result =
-              await _postcardService.claimEmptyPostcard(claimRequest);
-          final tokenID = 'tez-${result.contractAddress}-${result.tokenID}';
-          final token = AssetToken(
-            asset: Asset.init(
-              indexID: tokenID,
-              artistName: 'MoMa',
-              maxEdition: 1,
-              mimeType: 'image/png',
-              title: 'Postcard 001',
-              thumbnailURL: result.imageCID,
-              previewURL: result.imageCID,
-              source: 'postcard',
-            ),
-            blockchain: "tezos",
-            fungible: false,
-            contractType: '',
-            tokenId: result.tokenID,
-            contractAddress: result.contractAddress,
-            edition: 0,
-            editionName: "",
-            id: tokenID,
-            balance: 1,
-            owner: address,
-            lastActivityTime: DateTime.now(),
-            lastRefreshedTime: DateTime(1),
-            pending: true,
-            originTokenInfo: [],
-            provenance: [],
-            owners: {},
-          );
-          await _tokenService.setCustomTokens([token]);
-          await _tokenService.reindexAddresses([address]);
-          injector.get<ConfigurationService>().setListPostcardMint([tokenID]);
-          NftCollectionBloc.eventController.add(
-            GetTokensByOwnerEvent(pageKey: PageKey.init()),
-          );
+          try {
+            final claimRequest = ClaimPostCardRequest(
+              address: address,
+              id: 'postcard',
+              timestamp: timestamp,
+              publicKey: publicKey,
+              signature: signature,
+            );
+            final result =
+                await _postcardService.claimEmptyPostcard(claimRequest);
+            final tokenID = 'tez-${result.contractAddress}-${result.tokenID}';
+            final token = AssetToken(
+              asset: Asset.init(
+                indexID: tokenID,
+                artistName: 'MoMa',
+                maxEdition: 1,
+                mimeType: 'image/png',
+                title: 'Postcard 001',
+                thumbnailURL: result.imageCID,
+                previewURL: result.imageCID,
+                source: 'postcard',
+              ),
+              blockchain: "tezos",
+              fungible: false,
+              contractType: '',
+              tokenId: result.tokenID,
+              contractAddress: result.contractAddress,
+              edition: 0,
+              editionName: "",
+              id: tokenID,
+              balance: 1,
+              owner: address,
+              lastActivityTime: DateTime.now(),
+              lastRefreshedTime: DateTime(1),
+              pending: true,
+              originTokenInfo: [],
+              provenance: [],
+              owners: {},
+            );
+            await _tokenService.setCustomTokens([token]);
+            await _tokenService.reindexAddresses([address]);
+            injector.get<ConfigurationService>().setListPostcardMint([tokenID]);
+            NftCollectionBloc.eventController.add(
+              GetTokensByOwnerEvent(pageKey: PageKey.init()),
+            );
+          } catch (e) {
+            rethrow;
+          }
           emit(state.copyWith(isClaiming: false, isClaimed: true));
         }
       } on DioError catch (e) {
