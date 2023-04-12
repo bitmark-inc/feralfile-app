eb14416a7bf6cd06aa7afbd52bc5064b22c7e2f7
phuoc <phuoc@bitmark.com>
2023-04-12 16:25:51 +0700

    filter address
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/screen/chat/chat_thread_page.dart
+++ lib/screen/chat/chat_thread_page.dart
@@ -174,7 +174,9 @@ class _ChatThreadPageState extends State<ChatThreadPage> {
       lastMessageTimestamp = newMessages.last.timestamp;
       historyRequestId = null;
     } else {
-      messages.insertAll(0, _convertMessages(newMessages));
+      final otherPeopleMessages   =_convertMessages(newMessages);
+      otherPeopleMessages.removeWhere((element) => element.author.id == user.id);
+      messages.insertAll(0, otherPeopleMessages);
       messages.sort((a, b) => (b.createdAt ?? 0).compareTo(a.createdAt ?? 0));
     }
     setState(() {});
