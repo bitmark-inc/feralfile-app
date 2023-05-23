e91e53b718945cc9b24cc1884bd11b15b496cad1
phuoc <phuoc@bitmark.com>
2023-05-23 17:39:43 +0700

    swiper
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/screen/interactive_postcard/postcard_explain.dart
+++ lib/screen/interactive_postcard/postcard_explain.dart
@@ -5,14 +5,17 @@ import 'package:autonomy_flutter/util/asset_token_ext.dart';
 import 'package:autonomy_flutter/util/constants.dart';
 import 'package:autonomy_flutter/util/geolocation.dart';
 import 'package:autonomy_flutter/util/postcard_extension.dart';
+import 'package:autonomy_flutter/util/style.dart';
 import 'package:autonomy_flutter/view/back_appbar.dart';
 import 'package:autonomy_flutter/view/postcard_button.dart';
 import 'package:autonomy_flutter/view/postcard_explain.dart';
+import 'package:autonomy_flutter/view/primary_button.dart';
 import 'package:autonomy_flutter/view/responsive.dart';
 import 'package:autonomy_theme/autonomy_theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:nft_collection/models/asset_token.dart';
+import 'package:flutter_swiper_view/flutter_swiper_view.dart';
 
 import 'design_stamp.dart';
 
@@ -50,54 +53,17 @@ class _PostcardExplainState extends State<PostcardExplain> {
       ),
       body: Padding(
         padding: ResponsiveLayout.pageHorizontalEdgeInsetsWithSubmitButton,
-        child: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 20),
-                    PostcardExplainView(
-                      message: widget.payload.asset.postcardMetadata.isFinal
-                          ? "you_are_the_final_receiver".tr()
-                          : "",
-                      counter: widget.payload.asset.postcardMetadata.counter,
-                    ),
-                    PostcardButton(
-                      text: "next_design_your_stamp".tr(),
-                      onTap: () async {
-                        if (isGetLocation) {
-                          final location = await getGeoLocationWithPermission();
-                          if (!mounted || location == null) return;
-                          Navigator.of(context).pushNamed(AppRouter.designStamp,
-                              arguments: DesignStampPayload(
-                                  widget.payload.asset, location));
-                        } else {
-                          Navigator.of(context).pushNamed(AppRouter.designStamp,
-                              arguments: DesignStampPayload(
-                                  widget.payload.asset, null));
-                        }
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                            "game_runs_from_".tr(
-                              namedArgs: {
-                                "from": POSRCARD_GAME_START,
-                                "to": POSRCARD_GAME_END,
-                              },
-                            ),
-                            style: theme.textTheme.ppMori400Grey14),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ],
+        child: Swiper(
+          itemBuilder: (context, index) {
+            return Text(index.toString());
+          },
+          itemCount: 5,
+          pagination: const SwiperPagination(
+              builder: DotSwiperPaginationBuilder(
+                  color: AppColor.auLightGrey,
+                  activeColor: MomaPallet.lightYellow)),
+          control: const SwiperControl(),
+          loop: false,
         ),
       ),
     );
