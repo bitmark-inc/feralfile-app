f9a959a0fd4f2fbc63a5bc094bd89c38a10b318e
Nguyen Phuoc Sang <47568867+ppupha@users.noreply.github.com>
2023-06-02 14:11:59 +0700

    Update Default postcard cid (#993)
    * Modify postcard request api
    
    * Remove thumbnailUrl

--- lib/screen/interactive_postcard/claim_empty_postcard/claim_empty_postcard_bloc.dart
+++ lib/screen/interactive_postcard/claim_empty_postcard/claim_empty_postcard_bloc.dart
@@ -11,7 +11,6 @@ import 'package:autonomy_flutter/service/metric_client_service.dart';
 import 'package:autonomy_flutter/service/navigation_service.dart';
 import 'package:autonomy_flutter/service/postcard_service.dart';
 import 'package:autonomy_flutter/service/tezos_service.dart';
-import 'package:autonomy_flutter/util/constants.dart';
 import 'package:autonomy_flutter/util/wallet_storage_ext.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -35,9 +34,7 @@ class ClaimEmptyPostCardBloc
           mimeType: 'image/png',
           title: event.claimRequest.name,
           medium: 'software',
-          thumbnailURL: EMPTY_POSTCARD_PREVIEW_URL_IMAGE,
-          galleryThumbnailURL: EMPTY_POSTCARD_PREVIEW_URL_IMAGE,
-          previewURL: EMPTY_POSTCARD_PREVIEW_URL_IMAGE,
+          previewURL: event.claimRequest.previewURL,
         ),
         blockchain: "tezos",
         fungible: true,
@@ -127,8 +124,7 @@ class ClaimEmptyPostCardBloc
               maxEdition: 1,
               mimeType: 'image/png',
               title: event.claimRequest.name,
-              thumbnailURL: EMPTY_POSTCARD_PREVIEW_URL_IMAGE,
-              previewURL: EMPTY_POSTCARD_PREVIEW_URL_SOFTWARE,
+              previewURL: event.claimRequest.previewURL,
               source: 'postcard',
               artworkMetadata: jsonEncode(postcardMetadata.toJson()),
               medium: 'software',
