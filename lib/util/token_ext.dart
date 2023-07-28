6680b5ddf6388f7a234e5befea7b7b42026baeea
PuPha <nguyensanghso@gmail.com>
2023-07-28 17:31:23 +0700

    Update Number nft event

--- lib/util/token_ext.dart
+++ lib/util/token_ext.dart
@@ -5,14 +5,14 @@ import 'package:collection/collection.dart';
 import 'package:nft_collection/models/asset_token.dart';
 
 extension AssetTokenExtension on List<CompactedAssetToken> {
-  List<CompactedAssetToken> filterAssetToken() {
+  List<CompactedAssetToken> filterAssetToken({bool isShowHidden = false}) {
     final hiddenTokens =
         injector<ConfigurationService>().getTempStorageHiddenTokenIDs();
     final sentArtworks =
         injector<ConfigurationService>().getRecentlySentToken();
     final expiredTime = DateTime.now().subtract(SENT_ARTWORK_HIDE_TIME);
     return whereNot((element) =>
-        hiddenTokens.contains(element.id) ||
+        (!isShowHidden && hiddenTokens.contains(element.id)) ||
         ((element.balance ?? 0) <= 0 && element.isDebugged != true) ||
         sentArtworks.any(
           (e) =>
