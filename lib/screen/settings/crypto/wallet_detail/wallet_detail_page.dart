4a87dcdcbac273eaf29dd401dcdf586c1c84c6bc
phuoc <phuoc@bitmark.com>
2023-08-10 14:59:42 +0700

    get ether scan url
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/screen/settings/crypto/wallet_detail/wallet_detail_page.dart
+++ lib/screen/settings/crypto/wallet_detail/wallet_detail_page.dart
@@ -611,11 +611,11 @@ class _WalletDetailPageState extends State<WalletDetailPage> with RouteAware {
   String _txURL(String address, CryptoType cryptoType) {
     switch (cryptoType) {
       case CryptoType.ETH:
-        return "https://etherscan.io/address/$address";
+        return "$etherScanUrl/address/$address";
       case CryptoType.XTZ:
         return "https://tzkt.io/$address/operations";
       case CryptoType.USDC:
-        return "https://etherscan.io/token/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48?a=$address";
+        return "$etherScanUrl/token/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48?a=$address";
       default:
         return "";
     }
