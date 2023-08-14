e0a56851879ff79074f1b0b73dffc9fa71f166b0
phuoc <phuoc@bitmark.com>
2023-08-14 13:29:10 +0700

    delete editorial
    Signed-off-by: phuoc <phuoc@bitmark.com>

---
+++ lib/screen/discover/discover_page.dart
@@ -0,0 +1,101 @@
+//
+//  SPDX-License-Identifier: BSD-2-Clause-Patent
+//  Copyright © 2022 Bitmark. All rights reserved.
+//  Use of this source code is governed by the BSD-2-Clause Plus Patent License
+//  that can be found in the LICENSE file.
+//
+
+import 'package:autonomy_flutter/common/injector.dart';
+import 'package:autonomy_flutter/main.dart';
+import 'package:autonomy_flutter/screen/feed/feed_preview_page.dart';
+import 'package:autonomy_flutter/service/metric_client_service.dart';
+import 'package:autonomy_flutter/util/constants.dart';
+import 'package:autonomy_flutter/util/style.dart';
+import 'package:autonomy_flutter/view/back_appbar.dart';
+import 'package:autonomy_flutter/view/header.dart';
+import 'package:flutter/material.dart';
+
+class DiscoverPage extends StatefulWidget {
+  const DiscoverPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => DiscoverPageState();
+}
+
+class DiscoverPageState extends State<DiscoverPage>
+    with SingleTickerProviderStateMixin {
+  bool _showFullHeader = true;
+  late ScrollController _feedController;
+  final _metricClient = injector<MetricClientService>();
+
+  @override
+  void initState() {
+    super.initState();
+    memoryValues.homePageInitialTab = HomePageTab.HOME;
+    _feedController = ScrollController();
+    _feedController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    final isShowFullHeader = _feedController.offset < 80;
+    if (isShowFullHeader != _showFullHeader) {
+      setState(() {
+        _showFullHeader = isShowFullHeader;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _metricClient.addEvent(MixpanelEvent.timeViewDiscovery);
+    _feedController.dispose();
+    super.dispose();
+  }
+
+  void scrollToTop() {
+    _feedController.animateTo(0,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.fastOutSlowIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: getDarkEmptyAppBar(),
+      backgroundColor: theme.primaryColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          headDivider(),
+          Container(
+            padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
+            child: AnimatedSize(
+              duration: const Duration(milliseconds: 300),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                  mainAxisAlignment: _showFullHeader
+                      ? MainAxisAlignment.spaceBetween
+                      : MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    if (_showFullHeader)
+                      const AutonomyLogo(
+                        isWhite: true,
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FeedPreviewPage(
+              controller: _feedController,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/service/mix_panel_client_service.dart
+++ lib/service/mix_panel_client_service.dart
@@ -10,7 +10,6 @@ import 'package:autonomy_flutter/util/string_ext.dart';
 import 'package:autonomy_flutter/util/wallet_storage_ext.dart';
 import 'package:crypto/crypto.dart';
 import 'package:intl/intl.dart';
-import 'package:json_annotation/json_annotation.dart';
 import 'package:mixpanel_flutter/mixpanel_flutter.dart';
 
 class MixPanelClientService {
@@ -109,42 +108,5 @@ class MixPanelClientService {
 
   void setLabel(String prop, dynamic value) {
     mixpanel.getPeople().set(prop, value);
-  }
-
-  MixpanelConfig? getConfig() {
-    return _configurationService.getMixpanelConfig();
-  }
-
-  Future<void> setConfig(MixpanelConfig config) async {
-    await _configurationService.setMixpanelConfig(config);
-  }
-}
-
-@JsonSerializable()
-class MixpanelConfig {
-  final DateTime? editorialPeriodStart;
-  final double? totalEditorialReading;
-
-  MixpanelConfig({this.editorialPeriodStart, this.totalEditorialReading});
-
-  Map<String, dynamic> toJson() => {
-        "editorialPeriodStart": editorialPeriodStart?.toIso8601String(),
-        "totalEditorialReading": totalEditorialReading,
-      };
-
-  factory MixpanelConfig.fromJson(Map<String, dynamic> json) {
-    return MixpanelConfig(
-      editorialPeriodStart: DateTime.tryParse(json['editorialPeriodStart']),
-      totalEditorialReading: json['totalEditorialReading'],
-    );
-  }
-
-  MixpanelConfig copyWith(
-      {DateTime? editorialPeriodStart, double? totalEditorialReading}) {
-    return MixpanelConfig(
-      editorialPeriodStart: editorialPeriodStart ?? this.editorialPeriodStart,
-      totalEditorialReading:
-          totalEditorialReading ?? this.totalEditorialReading,
-    );
   }
 }
