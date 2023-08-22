d73cf621da260a26b1a86e7f0778d5f490fbe408
phuoc <phuoc@bitmark.com>
2023-08-22 16:26:54 +0700

    add button
    Signed-off-by: phuoc <phuoc@bitmark.com>

---
+++ lib/view/add_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AddButton extends StatelessWidget {
+  final Function() onTap;
+  final double size;
+
+  const AddButton({Key? key, required this.onTap, this.size = 20})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: onTap,
+        child: SvgPicture.asset(
+          "assets/images/add_icon.svg",
+          width: size,
+          height: size,
+        ));
+  }
+}
