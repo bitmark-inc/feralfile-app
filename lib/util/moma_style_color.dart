769be2ae65db20a18c62def2628c267a10700022
PuPha <nguyensanghso@gmail.com>
2023-09-08 09:48:33 +0700

    Update Postcard Detail Pop-up

--- lib/util/moma_style_color.dart
+++ lib/util/moma_style_color.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MoMAColors {
+  static const moMA3 = Color.fromRGBO(236, 100, 99, 1);
   static const moMA5 = Color.fromRGBO(240, 148, 62, 1);
   static const moMA12 = Color.fromRGBO(131, 79, 196, 1);
 }
