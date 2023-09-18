65b766c93804473a243dcada12e7237ec06cd2ba
Nguyen Phuoc Sang <47568867+ppupha@users.noreply.github.com>
2023-09-18 11:51:16 +0700

    Update Time Compare (#1253)
    * Update Time Compare
    
    * Fix Color

--- lib/util/moma_style_color.dart
+++ lib/util/moma_style_color.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MoMAColors {
+  static const moMA1 = Color.fromRGBO(215, 75, 168, 1);
   static const moMA3 = Color.fromRGBO(236, 100, 99, 1);
   static const moMA3Disable = Color.fromRGBO(245, 177, 177, 1);
   static const moMA5 = Color.fromRGBO(240, 148, 62, 1);
