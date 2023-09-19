4bbc204628be86d3fa5c6cfcfc9f5b4ee8c2be0d
PuPha <nguyensanghso@gmail.com>
2023-09-19 15:35:37 +0700

    use getUpdatedLinkedAccounts

--- lib/util/asset_token_ext.dart
+++ lib/util/asset_token_ext.dart
@@ -149,7 +149,8 @@ extension AssetTokenExtension on AssetToken {
   Future<bool> isViewOnly() async {
     final cloudDB = injector<CloudDatabase>();
     final walletAddress = await cloudDB.addressDao.findByAddress(owner);
-    final viewOnlyConnections = await cloudDB.connectionDao.getLinkedAccounts();
+    final viewOnlyConnections =
+        await cloudDB.connectionDao.getUpdatedLinkedAccounts();
     final connection = viewOnlyConnections.firstWhereOrNull(
         (viewOnlyConnection) => viewOnlyConnection.key == owner);
     return walletAddress == null && connection != null;
