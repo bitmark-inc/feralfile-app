a2a2bf1fd757211616317becad9320108bf1291e
PuPha <nguyensanghso@gmail.com>
2023-11-01 09:35:51 +0700

    Remove Rendering Report

--- lib/common/injector.dart
+++ lib/common/injector.dart
@@ -24,7 +24,6 @@ import 'package:autonomy_flutter/gateway/feralfile_api.dart';
 import 'package:autonomy_flutter/gateway/iap_api.dart';
 import 'package:autonomy_flutter/gateway/postcard_api.dart';
 import 'package:autonomy_flutter/gateway/pubdoc_api.dart';
-import 'package:autonomy_flutter/gateway/rendering_report_api.dart';
 import 'package:autonomy_flutter/gateway/tzkt_api.dart';
 import 'package:autonomy_flutter/screen/interactive_postcard/claim_empty_postcard/claim_empty_postcard_bloc.dart';
 import 'package:autonomy_flutter/screen/playlists/add_new_playlist/add_new_playlist_bloc.dart';
@@ -262,9 +261,6 @@ Future<void> setup() async {
             mainnetDB.draftCustomerSupportDao,
             CustomerSupportApi(authenticatedDio,
                 baseUrl: Environment.customerSupportURL),
-            RenderingReportApi(authenticatedDio,
-                baseUrl: Environment.renderingReportURL),
-            injector(),
             injector(),
             mainnetDB.announcementDao,
             AnnouncementApi(authenticatedDio,
