d424491eaf55b974b970eb6df440fbab0c380af1
PuPha <nguyensanghso@gmail.com>
2023-11-08 17:13:16 +0700

    Update

--- lib/screen/interactive_postcard/postcard_detail_page.dart
+++ lib/screen/interactive_postcard/postcard_detail_page.dart
@@ -605,53 +605,56 @@ class ClaimedPostcardDetailPageState extends State<ClaimedPostcardDetailPage>
         ),
       ),
     ];
-    if (!isSending && !asset.isFinal) {
-      timer?.cancel();
+    if (!asset.isFinal) {
+      if (!isSending) {
+        timer?.cancel();
 
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Builder(builder: (final context) {
-            final box = context.findRenderObject() as RenderBox?;
-            return PostcardAsyncButton(
-              text: "invite_to_collaborate".tr(),
-              color: MoMAColors.moMA8,
-              onTap: () async {
-                await asset.sharePostcard(
-                  onSuccess: () {
-                    setState(() {
-                      isSending = asset.isSending;
-                    });
-                  },
-                  onFailed: (e) {
-                    if (e is DioException) {
-                      if (mounted) {
-                        UIHelper.showSharePostcardFailed(context, e);
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Builder(builder: (final context) {
+              final box = context.findRenderObject() as RenderBox?;
+              return PostcardAsyncButton(
+                text: "invite_to_collaborate".tr(),
+                color: MoMAColors.moMA8,
+                onTap: () async {
+                  await asset.sharePostcard(
+                    onSuccess: () {
+                      setState(() {
+                        isSending = asset.isSending;
+                      });
+                    },
+                    onFailed: (e) {
+                      if (e is DioException) {
+                        if (mounted) {
+                          UIHelper.showSharePostcardFailed(context, e);
+                        }
                       }
-                    }
-                  },
-                  sharePositionOrigin:
-                      box!.localToGlobal(Offset.zero) & box.size,
-                );
-              },
-            );
-          }),
-          ...sendPostcardExplain,
-        ],
-      );
-    } else {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          PostcardButton(
-            text: "postcard_sent".tr(),
-            disabledColor: AppColor.momaGreen,
-            enabled: false,
-          ),
-          ...sendPostcardExplain,
-        ],
-      );
+                    },
+                    sharePositionOrigin:
+                        box!.localToGlobal(Offset.zero) & box.size,
+                  );
+                },
+              );
+            }),
+            ...sendPostcardExplain,
+          ],
+        );
+      } else {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            PostcardButton(
+              text: "postcard_sent".tr(),
+              disabledColor: AppColor.momaGreen,
+              enabled: false,
+            ),
+            ...sendPostcardExplain,
+          ],
+        );
+      }
     }
+    return const SizedBox();
   }
 
   Widget _postcardInfo(BuildContext context, AssetToken asset) {
