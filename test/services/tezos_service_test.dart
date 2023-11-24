f9d8b29d6ad84bed2c9fcd4b298db2ac42dc186a
phuoc <phuoc@bitmark.com>
2023-11-24 14:49:27 +0700

    lint
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- test/services/tezos_service_test.dart
+++ test/services/tezos_service_test.dart
@@ -7,12 +7,12 @@ import 'package:libauk_dart/libauk_dart.dart';
 import 'package:tezart/tezart.dart';
 import 'package:uuid/uuid.dart';
 
-main() {
-  final client = TezartClient("https://ghostnet.tezos.marigold.dev");
+void main() {
+  final client = TezartClient('https://ghostnet.tezos.marigold.dev');
   final tezosService = TezosServiceImpl(client);
   final walletStorage = MockWalletStorage(const Uuid().v4());
 
-  const publicKey = "edpkvB8a5H6uwbzKysXRzZ96EqT5pVouZFvz6Qye67sgcZFkSZS92x";
+  const publicKey = 'edpkvB8a5H6uwbzKysXRzZ96EqT5pVouZFvz6Qye67sgcZFkSZS92x';
 
   group('tezos service test', () {
     test('sign message', () async {
@@ -20,25 +20,25 @@ main() {
           walletStorage, 0, Uint8List.fromList(utf8.encode('message')));
 
       expect(message,
-          "edsigtXomBKi5CTRf5cjATJWSyaRvhfYNHqSUGrn4SdbYRcGwQrUGjzEfQDTuqHhuA8b2d8NarZjz8TRf65WkpQmo423BtomS8Q");
+          '''edsigtXomBKi5CTRf5cjATJWSyaRvhfYNHqSUGrn4SdbYRcGwQrUGjzEfQDTuqHhuA8b2d8NarZjz8TRf65WkpQmo423BtomS8Q''');
     });
 
     test('get tezos balance', () async {
       final balance1 =
-          await tezosService.getBalance("tz1R22Abp5iFPaCYUEBZ6WAQtNUjZXsu6ehT");
+          await tezosService.getBalance('tz1R22Abp5iFPaCYUEBZ6WAQtNUjZXsu6ehT');
       expect(balance1, greaterThan(0));
     });
 
     test('estimate transaction fee', () async {
       final estimate = await tezosService.estimateFee(
-          publicKey, "tz1iuLHmMKec8M8ZqaXXPmYhFiEiRxN86k44", 1);
+          publicKey, 'tz1iuLHmMKec8M8ZqaXXPmYhFiEiRxN86k44', 1);
 
       expect(estimate, greaterThan(0));
     });
 
     test('process transaction operation', () async {
       final operation = TransactionOperation(
-          amount: 1, destination: "tz1iuLHmMKec8M8ZqaXXPmYhFiEiRxN86k44");
+          amount: 1, destination: 'tz1iuLHmMKec8M8ZqaXXPmYhFiEiRxN86k44');
 
       final estimate =
           await tezosService.estimateOperationFee(publicKey, [operation]);
@@ -49,9 +49,9 @@ main() {
     test('process batch operation', () async {
       final operations = [
         TransactionOperation(
-            amount: 2, destination: "tz1iuLHmMKec8M8ZqaXXPmYhFiEiRxN86k44"),
+            amount: 2, destination: 'tz1iuLHmMKec8M8ZqaXXPmYhFiEiRxN86k44'),
         TransactionOperation(
-            amount: 1, destination: "tz1iuLHmMKec8M8ZqaXXPmYhFiEiRxN86k44"),
+            amount: 1, destination: 'tz1iuLHmMKec8M8ZqaXXPmYhFiEiRxN86k44'),
       ];
 
       final estimate =
@@ -72,7 +72,8 @@ main() {
     //       amount: 1, destination: "tz1L76GWnRL8ottK7veac96JPuArFLEhZeVa");
     //
     //   final id =
-    //       await tezosService.sendOperationTransaction(walletStorage, [operation]);
+    //       await tezosService.
+    //       sendOperationTransaction(walletStorage, [operation]);
     //
     //   expect(id?.isNotEmpty, true);
     // });
@@ -80,15 +81,19 @@ main() {
 }
 
 class MockWalletStorage extends WalletStorage {
-  MockWalletStorage(String uuid) : super(uuid);
+  MockWalletStorage(super.uuid);
 
   @override
-  Future<String> getTezosPublicKey({int index = 0}) async => 'edpkvB8a5H6uwbzKysXRzZ96EqT5pVouZFvz6Qye67sgcZFkSZS92x';
+  Future<String> getTezosPublicKey({int index = 0}) async =>
+      'edpkvB8a5H6uwbzKysXRzZ96EqT5pVouZFvz6Qye67sgcZFkSZS92x';
 
   @override
-  Future<Uint8List> tezosSignMessage(Uint8List message, {int index = 0}) async => Uint8List(64);
+  Future<Uint8List> tezosSignMessage(Uint8List message,
+          {int index = 0}) async =>
+      Uint8List(64);
 
   @override
   Future<Uint8List> tezosSignTransaction(String forgedHex,
-      {int index = 0}) async => Uint8List(64);
+          {int index = 0}) async =>
+      Uint8List(64);
 }
