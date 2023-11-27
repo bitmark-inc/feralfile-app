fd43de1ba8fce6395f74f559143f9be680b4c556
phuoc <phuoc@bitmark.com>
2023-11-27 15:16:56 +0700

    restore data setting earlier
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/screen/app_router.dart
+++ lib/screen/app_router.dart
@@ -257,6 +257,7 @@ class AppRouter {
                 injector<CloudDatabase>(),
                 injector(),
                 injector<AuditService>(),
+                injector(),
               ),
             ),
           ], child: const OnboardingPage()),
