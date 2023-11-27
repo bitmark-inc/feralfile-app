568341457d67c103a870aed809d57621574e2ac4
phuoc <phuoc@bitmark.com>
2023-11-27 13:30:09 +0700

    use constant string
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- test/gateway/activation_mock.dart
+++ test/gateway/activation_mock.dart
@@ -2,15 +2,21 @@ import 'package:autonomy_flutter/gateway/activation_api.dart';
 import 'package:dio/dio.dart';
 
 import 'api_mock_data.dart';
+import 'constants.dart';
 
 class ActivationApiMock {
   ///// getActivation
   static final MockData getActivationValid = MockData(
-      'activationIdValid',
+      activationIdValid,
       ActivationInfo(
-          'name', 'description', 'blockchain', 'contractAddress', 'tokenID'));
+        name,
+        description,
+        blockchain,
+        contractAddress,
+        tokenID,
+      ));
   static final MockData getActivationDioException4xx = MockData(
-      'activationIdDioException4xx',
+      activationIdDioException4xx,
       DioException(
           requestOptions: RequestOptions(path: 'path'),
           response: Response(
@@ -18,7 +24,7 @@ class ActivationApiMock {
               statusCode: 400,
               data: {'message': 'invalid id'})));
   static final MockData getActivationDioException5xx = MockData(
-      'activationIdDioException5xx',
+      activationIdDioException5xx,
       DioException(
           requestOptions: RequestOptions(path: 'path'),
           response: Response(
@@ -27,35 +33,36 @@ class ActivationApiMock {
               data: {'message': 'internal server error'})));
 
   static final MockData getActivationConnectionTimeout = MockData(
-      'activationIdConnectionTimeout',
+      activationIdConnectionTimeout,
       DioException(
           requestOptions: RequestOptions(path: 'path'),
           type: DioExceptionType.connectionTimeout,
           error: 'activationIdConnectionTimeout'));
 
   static final MockData getActivationReceiveTimeout = MockData(
-      'activationIdReceiveTimeout',
+      activationIdReceiveTimeout,
       DioException(
           requestOptions: RequestOptions(path: 'path'),
           type: DioExceptionType.receiveTimeout,
           error: 'activationIdReceiveTimeout'));
 
   static final MockData getActivationExceptionOther = MockData(
-      'activationIdDioExceptionOther', Exception('activationIdExceptionOther'));
+      activationIdExceptionOther, Exception('activationIdExceptionOther'));
 
   ///// claim
   static final MockData claimValid = MockData(
       ActivationClaimRequest(
-          activationID: 'activationIdValid',
-          address: 'addressValid',
-          airdropTOTPPasscode: 'airdropTOTPPasscodeValid'),
+        activationID: activationIdValid,
+        address: address,
+        airdropTOTPPasscode: airdropTOTPPasscode,
+      ),
       ActivationClaimResponse());
 
   static final MockData claimDioException4xx = MockData(
       ActivationClaimRequest(
-          activationID: 'activationIdDioException4xx',
-          address: 'address4xx',
-          airdropTOTPPasscode: 'airdropTOTPPasscode4xx'),
+          activationID: activationIdDioException4xx,
+          address: address,
+          airdropTOTPPasscode: airdropTOTPPasscode),
       DioException(
           requestOptions: RequestOptions(path: 'path'),
           response: Response(
@@ -65,9 +72,9 @@ class ActivationApiMock {
 
   static final MockData claimDioException5xx = MockData(
       ActivationClaimRequest(
-          activationID: 'activationIdDioException5xx',
-          address: 'address5xx',
-          airdropTOTPPasscode: 'airdropTOTPPasscode5xx'),
+          activationID: activationIdDioException5xx,
+          address: address,
+          airdropTOTPPasscode: airdropTOTPPasscode),
       DioException(
           requestOptions: RequestOptions(path: 'path'),
           response: Response(
@@ -77,9 +84,9 @@ class ActivationApiMock {
 
   static final MockData claimConnectionTimeout = MockData(
       ActivationClaimRequest(
-          activationID: 'activationIdConnectionTimeout',
-          address: 'addressConnectionTimeout',
-          airdropTOTPPasscode: 'airdropTOTPPasscodeConnectionTimeout'),
+          activationID: activationIdConnectionTimeout,
+          address: address,
+          airdropTOTPPasscode: airdropTOTPPasscode),
       DioException(
           requestOptions: RequestOptions(path: 'path'),
           type: DioExceptionType.connectionTimeout,
@@ -87,9 +94,9 @@ class ActivationApiMock {
 
   static final MockData claimReceiveTimeout = MockData(
       ActivationClaimRequest(
-          activationID: 'activationIdReceiveTimeout',
-          address: 'addressReceiveTimeout',
-          airdropTOTPPasscode: 'airdropTOTPPasscodeReceiveTimeout'),
+          activationID: activationIdReceiveTimeout,
+          address: address,
+          airdropTOTPPasscode: airdropTOTPPasscode),
       DioException(
           requestOptions: RequestOptions(path: 'path'),
           type: DioExceptionType.receiveTimeout,
@@ -97,8 +104,8 @@ class ActivationApiMock {
 
   static final MockData claimDioExceptionOther = MockData(
       ActivationClaimRequest(
-          activationID: 'activationIdDioExceptionOther',
-          address: 'addressDioExceptionOther',
-          airdropTOTPPasscode: 'airdropTOTPPasscodeDioExceptionOther'),
+          activationID: activationIdExceptionOther,
+          address: address,
+          airdropTOTPPasscode: airdropTOTPPasscode),
       Exception('claimExceptionOther'));
 }
---
+++ test/gateway/constants.dart
@@ -0,0 +1,14 @@
+const String activationIdValid = 'activationIdValid';
+const String activationIdDioException4xx = 'activationIdDioException4xx';
+const String activationIdDioException5xx = 'activationIdDioException5xx';
+const String activationIdConnectionTimeout = 'activationIdConnectionTimeout';
+const String activationIdReceiveTimeout = 'activationIdReceiveTimeout';
+const String activationIdExceptionOther = 'activationIdExceptionOther';
+
+const String name = 'name';
+const String description = 'description';
+const String blockchain = 'blockchain';
+const String contractAddress = 'contractAddress';
+const String tokenID = 'tokenID';
+const String address = 'address';
+const String airdropTOTPPasscode = 'airdropTOTPPasscode';
