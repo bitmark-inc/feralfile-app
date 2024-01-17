ea91bfd175a703e0f42459d352ad52e397c1e224
phuoc <phuoc@bitmark.com>
2024-01-17 14:05:25 +0700

    cache ff exhibition artwork thumbnail
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/view/ff_artwork_thumbnail_view.dart
+++ lib/view/ff_artwork_thumbnail_view.dart
@@ -1,5 +1,7 @@
 import 'package:autonomy_flutter/model/ff_account.dart';
 import 'package:autonomy_flutter/util/exhibition_ext.dart';
+import 'package:autonomy_flutter/view/artwork_common_widget.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class FFArtworkThumbnailView extends StatelessWidget {
@@ -11,13 +13,19 @@ class FFArtworkThumbnailView extends StatelessWidget {
   @override
   Widget build(BuildContext context) => GestureDetector(
         onTap: () => onTap?.call(),
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(artwork.thumbnailURL),
-              fit: BoxFit.cover,
+        child: CachedNetworkImage(
+          imageUrl: artwork.thumbnailURL,
+          imageBuilder: (context, imageProvider) => Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: imageProvider,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
+          placeholder: (context, url) => const GalleryThumbnailPlaceholder(),
+          errorWidget: (context, url, error) =>
+              const GalleryThumbnailErrorWidget(),
         ),
       );
 }
