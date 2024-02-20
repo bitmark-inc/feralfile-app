da99fa14f0c5bb4a459b1a7abf94986633b274a8
Hoang Btmrk <hoang@bitmark.com>
2024-02-20 10:37:03 +0700

    Remove tutorial video feature

--- lib/screen/customer_support/support_customer_page.dart
+++ lib/screen/customer_support/support_customer_page.dart
@@ -12,7 +12,6 @@ import 'package:autonomy_flutter/gateway/pubdoc_api.dart';
 import 'package:autonomy_flutter/main.dart';
 import 'package:autonomy_flutter/screen/app_router.dart';
 import 'package:autonomy_flutter/screen/customer_support/support_thread_page.dart';
-import 'package:autonomy_flutter/screen/customer_support/tutorial_videos_page.dart';
 import 'package:autonomy_flutter/service/configuration_service.dart';
 import 'package:autonomy_flutter/service/customer_support_service.dart';
 import 'package:autonomy_flutter/util/constants.dart';
@@ -113,7 +112,6 @@ class _SupportCustomerPageState extends State<SupportCustomerPage>
             addOnlyDivider(),
             _resourcesWidget(),
             _transactionHistoryTap(context, orderIds),
-            _videoTutorials(),
           ],
         ),
       ),
@@ -213,35 +211,5 @@ class _SupportCustomerPageState extends State<SupportCustomerPage>
         addOnlyDivider(),
       ],
     );
-  }
-
-  Widget _videoTutorials() {
-    final theme = Theme.of(context);
-    return FutureBuilder<List<VideoData>>(
-        // ignore: discarded_futures
-        future: injector<PubdocAPI>().getTutorialVideosFromGithub(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TappableForwardRow(
-                  leftWidget: Text('tutorial_videos'.tr(),
-                      style: theme.textTheme.ppMori400Black14),
-                  onTap: () async {
-                    await Navigator.of(context).pushNamed(
-                        AppRouter.tutorialVideoPage,
-                        arguments:
-                            TutorialVideosPayload(videos: snapshot.data!));
-                  },
-                  padding: ResponsiveLayout.tappableForwardRowEdgeInsets,
-                ),
-                addOnlyDivider(),
-              ],
-            );
-          } else {
-            return const SizedBox();
-          }
-        });
   }
 }
