1c45ec8dc091e72d377c19da13d4f3e77e68ac22
phuoc <phuoc@bitmark.com>
2024-03-12 09:43:32 +0700

    fix: fix event scrolling
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/view/event_view.dart
+++ lib/view/event_view.dart
@@ -41,58 +41,60 @@ class ExhibitionEventView extends StatelessWidget {
           borderRadius: BorderRadius.circular(8),
           color: AppColor.auGreyBackground,
         ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'event'.tr(),
-              style: theme.textTheme.ppMori400White12,
-            ),
-            const SizedBox(height: 30),
-            if (mediaImageUrl != null) ...[
-              Image.network(
-                mediaImageUrl,
-                fit: BoxFit.fitWidth,
-              ),
-              const SizedBox(height: 20),
-            ],
-            Text(exhibitionEvent.title,
-                style: theme.textTheme.ppMori400White14),
-            const SizedBox(height: 20),
-            if (exhibitionEvent.dateTime != null) ...[
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               Text(
-                'Date: ${dateFormat.format(exhibitionEvent.dateTime!)}',
-                style: theme.textTheme.ppMori400White14,
+                'event'.tr(),
+                style: theme.textTheme.ppMori400White12,
               ),
-              Text(
-                'Time: ${timeFormat.format(exhibitionEvent.dateTime!)}',
-                style: theme.textTheme.ppMori400White14,
-              ),
-            ],
-            if (exhibitionEvent.description != null) ...[
-              HtmlWidget(
-                exhibitionEvent.description!,
-                textStyle: theme.textTheme.ppMori400White14,
-              ),
+              const SizedBox(height: 30),
+              if (mediaImageUrl != null) ...[
+                Image.network(
+                  mediaImageUrl,
+                  fit: BoxFit.fitWidth,
+                ),
+                const SizedBox(height: 20),
+              ],
+              Text(exhibitionEvent.title,
+                  style: theme.textTheme.ppMori400White14),
               const SizedBox(height: 20),
-            ],
-            if (watchMoreUrl != null && watchMoreUrl.isNotEmpty)
-              GestureDetector(
-                onTap: () async {
-                  await Navigator.of(context).pushNamed(
-                      AppRouter.inappWebviewPage,
-                      arguments: InAppWebViewPayload(watchMoreUrl));
-                },
-                child: Text(
-                  'watch'.tr(),
-                  style: theme.textTheme.ppMori400White14.copyWith(
-                    decoration: TextDecoration.underline,
-                    decorationColor: AppColor.white,
+              if (exhibitionEvent.dateTime != null) ...[
+                Text(
+                  'Date: ${dateFormat.format(exhibitionEvent.dateTime!)}',
+                  style: theme.textTheme.ppMori400White14,
+                ),
+                Text(
+                  'Time: ${timeFormat.format(exhibitionEvent.dateTime!)}',
+                  style: theme.textTheme.ppMori400White14,
+                ),
+              ],
+              if (exhibitionEvent.description != null) ...[
+                HtmlWidget(
+                  exhibitionEvent.description!,
+                  textStyle: theme.textTheme.ppMori400White14,
+                ),
+                const SizedBox(height: 20),
+              ],
+              if (watchMoreUrl != null && watchMoreUrl.isNotEmpty)
+                GestureDetector(
+                  onTap: () async {
+                    await Navigator.of(context).pushNamed(
+                        AppRouter.inappWebviewPage,
+                        arguments: InAppWebViewPayload(watchMoreUrl));
+                  },
+                  child: Text(
+                    'watch'.tr(),
+                    style: theme.textTheme.ppMori400White14.copyWith(
+                      decoration: TextDecoration.underline,
+                      decorationColor: AppColor.white,
+                    ),
                   ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
