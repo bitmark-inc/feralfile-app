e5b4f5c933c5996495632140c4945c31e9426ce3
phuoc <phuoc@bitmark.com>
2024-03-18 14:32:23 +0700

    fix: apply for android only
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/util/secure_screen_channel.dart
+++ lib/util/secure_screen_channel.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:autonomy_flutter/util/log.dart';
 import 'package:flutter/services.dart';
@@ -7,8 +8,11 @@ class SecureScreenChannel {
   static const MethodChannel _channel = MethodChannel('secure_screen_channel');
 
   static void setSecureFlag(bool secure) {
+    if (!Platform.isAndroid) {
+      return;
+    }
     try {
-       unawaited(_channel.invokeMethod('setSecureFlag', {'secure': secure}));
+      unawaited(_channel.invokeMethod('setSecureFlag', {'secure': secure}));
     } catch (e) {
       log.info('Error setting secure flag: $e');
     }
