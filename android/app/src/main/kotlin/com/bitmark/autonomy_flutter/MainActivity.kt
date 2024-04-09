d2e6586396ee205c18e838feea9b502c544873aa
phuoc <phuoc@bitmark.com>
2024-04-09 10:32:37 +0700

    feat: deny android rooted device
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- android/app/src/main/kotlin/com/bitmark/autonomy_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/bitmark/autonomy_flutter/MainActivity.kt
@@ -11,13 +11,15 @@ import android.app.KeyguardManager
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
+import android.view.WindowManager.LayoutParams
+import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.biometric.BiometricManager
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import android.view.WindowManager.LayoutParams
-import android.os.Build
+import com.scottyab.rootbeer.RootBeer
 
 class MainActivity : FlutterFragmentActivity() {
     companion object {
@@ -31,6 +33,13 @@ class MainActivity : FlutterFragmentActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         FileLogger.init(applicationContext)
+        // Create a RootBeer instance
+        val rootBeer = RootBeer(this)
+        if (rootBeer.isRooted) {
+            Toast.makeText(this, "This app cannot be used on rooted devices.", Toast.LENGTH_SHORT)
+                .show()
+            finish() // Close the app
+        }
         MethodChannel(
             flutterEngine.dartExecutor.binaryMessenger,
             CHANNEL
