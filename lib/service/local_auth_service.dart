6121021caa41bbe6ebb0185aa6ca1c9d49d156aa
phuoc <phuoc@bitmark.com>
2024-04-17 14:16:52 +0700

    feat: track device authen fail multiple times
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/service/local_auth_service.dart
+++ lib/service/local_auth_service.dart
@@ -1,11 +1,42 @@
+import 'dart:async';
+
 import 'package:autonomy_flutter/common/injector.dart';
 import 'package:autonomy_flutter/service/configuration_service.dart';
 import 'package:autonomy_flutter/util/biometrics_util.dart';
 import 'package:autonomy_flutter/util/log.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:local_auth/local_auth.dart';
+import 'package:sentry/sentry_io.dart';
 
 class LocalAuthenticationService {
+  static final LocalAuthentication _localAuth = LocalAuthentication();
+  static int _count = 0;
+  static Timer? _resetTimer;
+  static const int _maxCount = 3;
+
+  static Future<bool> authenticate({required String localizedReason}) async {
+    final result = await _localAuth.authenticate(
+      localizedReason: localizedReason,
+    );
+    _resetTimer?.cancel();
+    if (result) {
+      _count = 0;
+    } else {
+      _count++;
+      if (_count >= _maxCount) {
+        // capture sentry event
+        log.info('Local auth failed $_count times');
+        unawaited(Sentry.captureMessage('Local auth failed $_count times'));
+        _count = 0;
+      } else {
+        _resetTimer = Timer(const Duration(minutes: 1), () {
+          _count = 0;
+        });
+      }
+    }
+    return result;
+  }
+
   static Future<bool> checkLocalAuth() async {
     final configurationService = injector<ConfigurationService>();
     final isAvailable = await authenticateIsAvailable();
@@ -13,10 +44,9 @@ class LocalAuthenticationService {
         configurationService.isDevicePasscodeEnabled();
 
     if (isDevicePasscodeEnabled && isAvailable) {
-      final localAuth = LocalAuthentication();
       bool didAuthenticate = false;
       try {
-        didAuthenticate = await localAuth.authenticate(
+        didAuthenticate = await _localAuth.authenticate(
           localizedReason: 'authen_for_autonomy'.tr(),
         );
       } catch (e) {
