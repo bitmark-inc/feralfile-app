6f9c922e1ad4ad66a2ac6c1c8cfa24b7b5215684
phuoc <phuoc@bitmark.com>
2024-05-10 10:57:38 +0700

    lint
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/screen/playlists/view_playlist/view_playlist.dart
+++ lib/screen/playlists/view_playlist/view_playlist.dart
@@ -26,7 +26,6 @@ import 'package:autonomy_flutter/view/back_appbar.dart';
 import 'package:autonomy_flutter/view/responsive.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:feralfile_app_theme/feral_file_app_theme.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
