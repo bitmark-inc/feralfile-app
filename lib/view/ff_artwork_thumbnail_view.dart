906d365b05bd598960ec4a3df4a49e5b0604fae2
phuocbitmark <110799144+phuocbitmark@users.noreply.github.com>
2024-05-10 13:42:09 +0700

    Update lib/view/ff_artwork_thumbnail_view.dart
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/view/ff_artwork_thumbnail_view.dart
+++ lib/view/ff_artwork_thumbnail_view.dart
@@ -2,7 +2,6 @@ import 'package:autonomy_flutter/model/ff_account.dart';
 import 'package:autonomy_flutter/util/exhibition_ext.dart';
 import 'package:autonomy_flutter/view/artwork_common_widget.dart';
 import 'package:autonomy_flutter/view/image_background.dart';
-import 'package:feralfile_app_theme/feral_file_app_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
