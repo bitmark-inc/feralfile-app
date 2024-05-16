b3a4dd406af5f2b0ac7aac349f29809f90ba0efc
phuoc <phuoc@bitmark.com>
2024-05-16 14:27:19 +0700

    lint
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/view/get_started_banner.dart
+++ lib/view/get_started_banner.dart
@@ -2,10 +2,7 @@ import 'package:autonomy_flutter/util/au_icons.dart';
 import 'package:autonomy_flutter/view/primary_button.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:feralfile_app_theme/feral_file_app_theme.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class GetStartedBanner extends StatelessWidget {
   final Function? onClose;
