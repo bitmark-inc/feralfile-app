4845ffbe0dbbe79298b0e27a8f73a343032040f3
phuoc <phuoc@bitmark.com>
2024-05-22 16:04:23 +0700

    lint
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/screen/exhibitions/exhibitions_page.dart
+++ lib/screen/exhibitions/exhibitions_page.dart
@@ -19,7 +19,6 @@ import 'package:collection/collection.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:feralfile_app_theme/feral_file_app_theme.dart';
 import 'package:feralfile_app_tv_proto/feralfile_app_tv_proto.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
