bf9afeef20a0bc6f53f988dd24809e29ee55be4d
Hoang Btmrk <hoang@bitmark.com>
2024-06-06 15:36:46 +0700

    fix: remove isAppCenterBuild condition

--- lib/service/iap_service.dart
+++ lib/service/iap_service.dart
@@ -261,8 +261,7 @@ class IAPServiceImpl implements IAPService {
   @override
   Future<bool> isSubscribed() async {
     final jwt = _configurationService.getIAPJWT();
-    return (jwt != null && jwt.isValid(withSubscription: true)) ||
-        await isAppCenterBuild();
+    return jwt != null && jwt.isValid(withSubscription: true);
   }
 
   Future _cleanupPendingTransactions() async {
