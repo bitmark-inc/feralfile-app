0ceade49de125fb1713fbd68a71bd1d5fe92bfe2
Hoang Btmrk <hoang@bitmark.com>
2024-06-07 14:28:14 +0700

    fix: remove year on series title for John Exhibition

--- lib/util/series_ext.dart
+++ lib/util/series_ext.dart
@@ -1,9 +1,12 @@
 import 'package:autonomy_flutter/model/ff_series.dart';
+import 'package:autonomy_flutter/util/constants.dart';
 
 extension FFSeriesExt on FFSeries {
   String get displayTitle {
     final year = mintedAt?.year ?? createdAt?.year;
-    return year != null ? '$title ($year)' : title;
+    return (year != null && exhibitionID != JOHN_GERRARD_EXHIBITION_ID)
+        ? '$title ($year)'
+        : title;
   }
 
   String get galleryURL => (metadata?['galleryURL'] ?? '') as String;
