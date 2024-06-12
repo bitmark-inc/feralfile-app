f34c7c557ee6351604918bb97ef749f7180e79fd
phuoc <phuoc@bitmark.com>
2024-06-12 09:21:04 +0700

    fix: list series include first artwork
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/service/feralfile_service.dart
+++ lib/service/feralfile_service.dart
@@ -505,11 +505,31 @@ class FeralFileServiceImpl extends FeralFileService {
   @override
   Future<List<FFSeries>> getListSeries(String exhibitionId,
       {bool includeFirstArtwork = false}) async {
+    if (exhibitionId == SOURCE_EXHIBITION_ID) {
+      final exhibition = await getSourceExhibition();
+      return exhibition.series ?? [];
+    }
     final response = await _feralFileApi.getListSeries(
         exhibitionID: exhibitionId,
         sortBy: 'displayIndex',
         sortOrder: 'ASC',
         includeFirstArtwork: includeFirstArtwork);
+    if (includeFirstArtwork &&
+        response.result.any((element) => element.artwork == null)) {
+      final series = response.result.first;
+      final fakeArtwork = await _getFakeSeriesArtworks(
+        await getExhibition(exhibitionId),
+        series,
+        0,
+        1,
+      );
+      return response.result.map((e) {
+        if (e.id == series.id) {
+          return e.copyWith(artwork: fakeArtwork.first);
+        }
+        return e;
+      }).toList();
+    }
     return response.result;
   }
 
