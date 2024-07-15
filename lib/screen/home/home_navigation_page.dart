3c3eb3262046481e46e4dfc168395b083f94085f
phuoc <phuoc@bitmark.com>
2024-07-15 15:17:34 +0700

    fix query address
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- lib/screen/home/home_navigation_page.dart
+++ lib/screen/home/home_navigation_page.dart
@@ -146,6 +146,7 @@ class HomeNavigationPageState extends State<HomeNavigationPage>
 
   Future<void> openCollection() async {
     await _onItemTapped(HomeNavigatorTab.collection.index);
+    _collectionHomePageKey.currentState?.scrollToTop();
   }
 
   Future<void> _onItemTapped(int index) async {
