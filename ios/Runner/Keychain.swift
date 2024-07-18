a1076a881d74e4aa49cec10a92d60376c7352414
PuPha <nguyensanghso@gmail.com>
2024-07-18 11:16:44 +0700

    fix: migrate

--- ios/Runner/Keychain.swift
+++ ios/Runner/Keychain.swift
@@ -81,6 +81,7 @@ class Keychain {
         let query = [
             kSecClass as String: kSecClassGenericPassword,
             kSecReturnData as String: kCFBooleanTrue!,
+            kSecAttrSynchronizable as String: kSecAttrSynchronizableAny,
             kSecReturnAttributes as String : kCFBooleanTrue,
             kSecMatchLimit as String: kSecMatchLimitAll,
             kSecAttrAccessGroup as String: Constant.keychainGroup
