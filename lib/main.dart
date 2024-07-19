6ad0fb6aa9024ccbaca64844ce09b1881c1a2e2c
PuPha <nguyensanghso@gmail.com>
2024-07-19 15:41:09 +0700

    fix: add passpharse when generate seedPublicData

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,7 @@ void main() async {
         try {
           await runFeralFileApp();
         } catch (e, stackTrace) {
+          log.info('Error in main', e);
           await Sentry.captureException(e, stackTrace: stackTrace);
           rethrow;
         }
