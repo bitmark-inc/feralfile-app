b1f6000f69f5ac1194b26658ecb28e3808e60e83
Hoang Btmrk <hoang@bitmark.com>
2024-09-19 10:08:12 +0700

    fix: remove vietnamese

--- lib/main.dart
+++ lib/main.dart
@@ -165,7 +165,7 @@ Future<void> _setupApp() async {
 
   runApp(
     EasyLocalization(
-      supportedLocales: const [Locale('en', 'US'), Locale('vi'), Locale('ja')],
+      supportedLocales: const [Locale('en', 'US'), Locale('ja')],
       path: 'assets/translations',
       fallbackLocale: const Locale('en', 'US'),
       child: const OverlaySupport.global(
