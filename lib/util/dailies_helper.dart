0ebef57040cab4ab356a780d837ff713522184fe
Hoang Btmrk <hoang@bitmark.com>
2024-09-20 13:35:53 +0700

    feat: add daily schedule time to remote config

--- lib/util/dailies_helper.dart
+++ lib/util/dailies_helper.dart
@@ -1,11 +1,18 @@
+import 'package:autonomy_flutter/common/injector.dart';
 import 'package:autonomy_flutter/model/dailies.dart';
+import 'package:autonomy_flutter/service/remote_config_service.dart';
 import 'package:collection/collection.dart';
 
 class DailiesHelper {
   static List<DailyToken> _dailies = [];
 
   static DailyToken? get currentDailies {
-    final now = DateTime.now().subtract(const Duration(hours: 6));
+    const defaultScheduleTime = 6;
+    final configScheduleTime = injector<RemoteConfigService>()
+        .getConfig<String>(ConfigGroup.daily, ConfigKey.scheduleTime,
+            defaultScheduleTime.toString());
+    final now =
+        DateTime.now().subtract(Duration(hours: int.parse(configScheduleTime)));
     return _dailies
         .lastWhereOrNull((element) => element.displayTime.isBefore(now));
   }
