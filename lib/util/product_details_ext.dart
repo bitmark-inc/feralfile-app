d2ab546daf8f7bebf9928a64f40e324a1beae873
PuPha <nguyensanghso@gmail.com>
2024-10-10 14:09:41 +0700

    Update

--- lib/util/product_details_ext.dart
+++ lib/util/product_details_ext.dart
@@ -2,41 +2,17 @@ import 'dart:io';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
-import 'package:in_app_purchase_android/in_app_purchase_android.dart';
-import 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart';
 import 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
 
 extension ProductDetailsExt on ProductDetails {
   static const _indiaCurrencyCode = 'INR';
 
-  SKSubscriptionPeriodUnit get period {
-    if (Platform.isAndroid) {
-      return SKSubscriptionPeriodUnit.year;
-    } else if (Platform.isIOS) {
-      return (this as AppStoreProductDetails)
-              .skProduct
-              .subscriptionPeriod
-              ?.unit ??
-          SKSubscriptionPeriodUnit.year;
-    } else {
-      throw Exception('Unsupported platform');
-    }
-  }
+  SKSubscriptionPeriodUnit get period => SKSubscriptionPeriodUnit.year;
 
   String get renewPolicyText {
     if (Platform.isAndroid && currencyCode == _indiaCurrencyCode) {
       return 'renew_policy_india'.tr();
     }
     return 'auto_renews_unless_cancelled'.tr();
-  }
-
-  String get price {
-    if (Platform.isAndroid) {
-      return (this as GooglePlayProductDetails).price;
-    } else if (Platform.isIOS) {
-      return (this as AppStoreProductDetails).skProduct.price;
-    } else {
-      throw Exception('Unsupported platform');
-    }
   }
 }
