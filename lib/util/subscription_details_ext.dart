304c219a49e8de2f7055237de284b893b1a54708
PuPha <nguyensanghso@gmail.com>
2024-10-17 11:36:18 +0700

    fix price for web purchase

--- lib/util/subscription_details_ext.dart
+++ lib/util/subscription_details_ext.dart
@@ -1,10 +1,13 @@
 import 'package:autonomy_flutter/screen/settings/subscription/upgrade_state.dart';
+import 'package:autonomy_flutter/service/iap_service.dart';
 
 extension ListSubscriptionDetails on List<SubscriptionDetails> {
   List<SubscriptionDetails> get activeSubscriptionDetails {
     final activeSubscriptionDetails = <SubscriptionDetails>[];
     for (final subscriptionDetail in this) {
-      activeSubscriptionDetails.add(subscriptionDetail);
+      if (subscriptionDetail.status == IAPProductStatus.completed) {
+        activeSubscriptionDetails.add(subscriptionDetail);
+      }
     }
     return activeSubscriptionDetails;
   }
