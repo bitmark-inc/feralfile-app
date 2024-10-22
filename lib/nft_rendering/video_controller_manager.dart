838d1882faedfc96653f0803dd01906b8acbc837
PuPha <nguyensanghso@gmail.com>
2024-10-22 18:24:47 +0700

    fix lint

--- lib/nft_rendering/video_controller_manager.dart
+++ lib/nft_rendering/video_controller_manager.dart
@@ -34,8 +34,8 @@ class VideoControllerManager {
       {bool resetSeekPosition = false}) async {
     final videoPath = videoUri.toString();
     if (_cyclingUriMap[videoPath] == true) {
-      log.info(
-          '[VideoControllerManager] Recycling video controller for $videoUri is already in progress');
+      log.info('[VideoControllerManager] Recycling video controller '
+          'for $videoUri is already in progress');
       return;
     }
     _cyclingUriMap[videoPath] = true;
@@ -54,8 +54,9 @@ class VideoControllerManager {
         try {
           _beforeVideoControllerDisposedHandlers[videoPath]?.call(controller);
         } catch (error) {
-          log.info(
-              '[VideoControllerManager] Error calling beforeVideoControllerDisposed handler for $videoUri: $error');
+          log.info('[VideoControllerManager] Error calling '
+              'beforeVideoControllerDisposed handler '
+              'for $videoUri: $error');
         }
         await controller.dispose();
         _videoControllers.remove(videoPath);
@@ -63,13 +64,14 @@ class VideoControllerManager {
       }
       _cyclingUriMap.remove(videoPath);
     } catch (error) {
-      log.info(
-          '[VideoControllerManager] Error recycling video controller for $videoUri: $error');
+      log.info('[VideoControllerManager] Error recycling video '
+          'controller for $videoUri: $error');
       _cyclingUriMap.remove(videoPath);
     }
   }
 
-  /// Disposes of a specific controller by [videoUri], or all if [videoUri] is null
+  /// Disposes of a specific controller by [videoUri],
+  /// or all if [videoUri] is null
   Future<void> recycle({Uri? videoUri, bool resetSeekPosition = false}) async {
     if (videoUri != null) {
       await _recycleUri(videoUri, resetSeekPosition: resetSeekPosition);
@@ -110,8 +112,8 @@ class VideoControllerManager {
     try {
       // Check if the controller for this videoUri is already in the pool
       if (_videoControllers.containsKey(videoPath)) {
-        log.info(
-            '[VideoControllerManager] Video controller for $videoUri already exists');
+        log.info('[VideoControllerManager] Video controller '
+            'for $videoUri already exists');
         // Bring the videoUri to the end of the pool (most recently used)
         _controllerPool
           ..remove(videoPath)
@@ -128,14 +130,14 @@ class VideoControllerManager {
         VideoPlayerController controller;
 
         if (_controllerPool.length < maxVideoControllers) {
-          log.info(
-              '[VideoControllerManager] Creating new video controller for $videoUri');
+          log.info('[VideoControllerManager] Creating new '
+              'video controller for $videoUri');
           // Create a new controller
           controller = VideoPlayerController.networkUrl(videoUri);
           await controller.initialize();
         } else {
-          log.info(
-              '[VideoControllerManager] Replacing video controller for $videoUri');
+          log.info('[VideoControllerManager] Replacing video '
+              'controller for $videoUri');
           // Replace the first (oldest) controller in the pool
           final oldestVideoPath = _controllerPool[0];
 
@@ -159,15 +161,15 @@ class VideoControllerManager {
         }
 
         onVideoControllerCreated?.call(controller);
-        log.info(
-            '[VideoControllerManager] Requested video controller for $videoUri');
+        log.info('[VideoControllerManager] '
+            'Requested video controller for $videoUri');
         completer.complete(controller);
         _requestVideoControllerCompleters.remove(videoPath);
         return controller;
       }
     } catch (error) {
-      log.info(
-          '[VideoControllerManager] Error requesting video controller for $videoUri: $error');
+      log.info('[VideoControllerManager] '
+          'Error requesting video controller for $videoUri: $error');
       completer.completeError(error);
       _requestVideoControllerCompleters.remove(videoPath);
       return completer.future;
