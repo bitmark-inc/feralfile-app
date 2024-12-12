76692771606f0adf29c846508694de4935c31665
Anh Nguyen <anhnguyen@bitmark.com>
2024-12-12 09:19:18 +0700

    remove [modified] text on title

--- ios/notification/NotificationService.swift
+++ ios/notification/NotificationService.swift
@@ -11,18 +11,6 @@ class NotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
-
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
-    }
     
     override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
