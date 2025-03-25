5c1a87a6de6ada7c111850a8caf84d424cd5c23c
PuPha <nguyensanghso@gmail.com>
2025-03-25 11:57:31 +0700

    Fix subscription page

--- lib/util/product_details_ext.dart
+++ lib/util/product_details_ext.dart
@@ -26,36 +26,100 @@ extension ProductDetailsExt on ProductDetails {
             ],
           ),
         ),
+        const SizedBox(height: 12),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Important (Regional Policy Notice)',
+              style: theme.textTheme.ppMori700Black14.copyWith(fontSize: 12),
+            ),
+            const SizedBox(height: 12),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(width: 4),
+                  Expanded(
+                    child: RichText(
+                      maxLines: 10,
+                      overflow: TextOverflow.ellipsis,
+                      text: TextSpan(
+                        style: theme.textTheme.ppMori400Black12,
+                        children: [
+                          TextSpan(
+                            text: 'India:',
+                            style: theme.textTheme.ppMori400Black12
+                                .copyWith(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: ' Automatic renewal is ',
+                          ),
+                          TextSpan(
+                            text: 'not available',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text:
+                                ' for subscriptions above ₹15,000. Subscribers in India must manually renew their subscription each year through their Google Play account.',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 12),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: RichText(
+                        maxLines: 3,
+                        text: TextSpan(
+                          style: theme.textTheme.ppMori400Black12,
+                          children: [
+                            TextSpan(
+                              text: 'All other regions:',
+                              style: theme.textTheme.ppMori400Black12
+                                  .copyWith(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(
+                              text:
+                                  ' Your subscription automatically renews each year unless canceled.',
+                            ),
+                          ],
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
         const SizedBox(height: 8),
         RichText(
-            textAlign: TextAlign.center,
             text: TextSpan(
-              style: theme.textTheme.ppMori400Black12,
-              children: [
-                TextSpan(
-                    text: 'Note:',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(
-                  text: ' A subscription is ',
-                ),
-                TextSpan(
-                    text: 'not required',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(
-                  text:
-                      ' to use the basic features of this app. Premium features require a subscription.',
-                ),
-              ],
-            )),
-        ...[
-          const SizedBox(height: 8),
-          Text(
-            textAlign: TextAlign.center,
-            'renew_policy_india'.tr(),
-            style: theme.textTheme.ppMori400Black12
-                .copyWith(fontWeight: FontWeight.bold),
-          ),
-        ],
+          style: theme.textTheme.ppMori400Black12,
+          children: [
+            TextSpan(
+                text: 'Note:', style: TextStyle(fontWeight: FontWeight.bold)),
+            TextSpan(
+              text: ' A subscription is ',
+            ),
+            TextSpan(
+                text: 'not required',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            TextSpan(
+              text:
+                  ' to use the basic features of this app. Premium features require a subscription.',
+            ),
+          ],
+        )),
+        ...[],
       ],
     );
   }
