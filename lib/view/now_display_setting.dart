d2e2c7e716fc2053bafafefc066b342da901025d
Hoang Btmrk <hoang@bitmark.com>
2025-04-03 14:57:38 +0700

    fix: update function rotate in now display setting

--- lib/view/now_display_setting.dart
+++ lib/view/now_display_setting.dart
@@ -2,7 +2,10 @@ import 'package:autonomy_flutter/common/injector.dart';
 import 'package:autonomy_flutter/model/bluetooth_device_status.dart';
 import 'package:autonomy_flutter/model/canvas_cast_request_reply.dart';
 import 'package:autonomy_flutter/model/canvas_device_info.dart';
+import 'package:autonomy_flutter/model/display_settings.dart';
 import 'package:autonomy_flutter/screen/app_router.dart';
+import 'package:autonomy_flutter/screen/bloc/artist_artwork_display_settings/artist_artwork_display_setting_bloc.dart';
+import 'package:autonomy_flutter/screen/device_setting/bluetooth_connected_device_config.dart';
 import 'package:autonomy_flutter/service/bluetooth_service.dart';
 import 'package:autonomy_flutter/service/canvas_client_service_v2.dart';
 import 'package:autonomy_flutter/util/log.dart';
@@ -23,6 +26,7 @@ class NowDisplaySettingView extends StatefulWidget {
 
 class _NowDisplaySettingViewState extends State<NowDisplaySettingView> {
   late ArtFraming selectedFitment;
+  late ScreenOrientation currentOrientation;
   late FFBluetoothDevice? connectedDevice;
   late BluetoothDeviceStatus? settings;
 
@@ -31,6 +35,17 @@ class _NowDisplaySettingViewState extends State<NowDisplaySettingView> {
     super.initState();
     settings = injector<FFBluetoothService>().bluetoothDeviceStatus.value;
     selectedFitment = settings?.artFraming ?? ArtFraming.fitToScreen;
+    currentOrientation = [
+      ScreenOrientation.landscape,
+      ScreenOrientation.landscapeReverse,
+    ].contains(
+      injector<FFBluetoothService>()
+          .bluetoothDeviceStatus
+          .value
+          ?.screenRotation,
+    )
+        ? ScreenOrientation.landscape
+        : ScreenOrientation.portrait;
     connectedDevice = injector<FFBluetoothService>().castingBluetoothDevice;
   }
 
@@ -90,6 +105,23 @@ class _NowDisplaySettingViewState extends State<NowDisplaySettingView> {
           try {
             await injector<CanvasClientServiceV2>()
                 .rotateCanvas(connectedDevice!);
+            final newOrientation =
+                currentOrientation == ScreenOrientation.portrait
+                    ? ScreenOrientation.landscape
+                    : ScreenOrientation.portrait;
+
+            await injector<CanvasClientServiceV2>().updateDisplaySettings(
+              connectedDevice!,
+              DisplaySettings(
+                tokenId: widget.tokenId,
+                setting: ArtistDisplaySetting(
+                  screenOrientation: newOrientation,
+                ),
+              ),
+            );
+            setState(() {
+              currentOrientation = newOrientation;
+            });
           } catch (e) {
             log.warning('NowDisplaySetting: updateDisplaySettings error: $e');
           }
