d4ed9650f0d5cfec3f4a3322193dfee6995bfe0e
Hoang Btmrk <hoang@bitmark.com>
2025-06-24 05:40:10 +0700

    feat: implement design tokens

---
+++ src/assets/design-tokens/tokens.dart
@@ -0,0 +1,137 @@
+
+//
+// tokens.dart
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+
+import 'dart:ui';
+
+class Tokens {
+    Tokens._();
+
+    static const black = Color(0xFF000000);
+    static const bodyFontFamily = 'PP Mori';
+    static const bodyFontSize = 20;
+    static const bodyFontWeight = 400;
+    static const bodyLetterSpacing = 0;
+    static const bodyLineHeight = 1.3;
+    static const bodyParagraphIndent = 0;
+    static const bodyParagraphSpacing = 0;
+    static const bodyTextCase = null;
+    static const bodyTextDecoration = null;
+    static const chatFontFamily = 'Space Mono';
+    static const chatFontSize = 14;
+    static const chatFontWeight = 400;
+    static const chatLetterSpacing = 0;
+    static const chatLineHeight = 1.2;
+    static const chatMobileFontFamily = 'Space Mono';
+    static const chatMobileFontSize = 12;
+    static const chatMobileFontWeight = 400;
+    static const chatMobileLetterSpacing = 0;
+    static const chatMobileLineHeight = 1.2;
+    static const chatMobileParagraphIndent = 0;
+    static const chatMobileParagraphSpacing = 0;
+    static const chatMobileTextCase = null;
+    static const chatMobileTextDecoration = null;
+    static const chatParagraphIndent = 0;
+    static const chatParagraphSpacing = 0;
+    static const chatTextCase = null;
+    static const chatTextDecoration = null;
+    static const collecting = Color(0xFFB9E5FF);
+    static const darkDarkGrey = Color(0xFF232323);
+    static const darkGrey = Color(0xFF4A4A4A);
+    static const fontFamiliesPpMori = 'PP Mori';
+    static const fontFamiliesSpaceMono = 'Space Mono';
+    static const fontSize0 = 20;
+    static const fontSize1 = 16;
+    static const fontSize2 = 48;
+    static const fontSize3 = 14;
+    static const fontSize4 = 12;
+    static const fontSize5 = 28;
+    static const fontSize6 = 16;
+    static const fontSize7 = 12;
+    static const fontWeightsPpMori0 = 400;
+    static const fontWeightsSpaceMono1 = 400;
+    static const h1FontFamily = 'PP Mori';
+    static const h1FontSize = 48;
+    static const h1FontWeight = 400;
+    static const h1LetterSpacing = 0;
+    static const h1LineHeight = 1.1;
+    static const h1ParagraphIndent = 0;
+    static const h1ParagraphSpacing = 0;
+    static const h1TextCase = null;
+    static const h1TextDecoration = null;
+    static const highlight = Color(0xFFECFF0C);
+    static const letterSpacing0 = 0;
+    static const letterSpacing1 = 0;
+    static const letterSpacing2 = 0;
+    static const letterSpacing3 = 0;
+    static const letterSpacing4 = 0;
+    static const letterSpacing5 = 0;
+    static const letterSpacing6 = 0;
+    static const letterSpacing7 = 0;
+    static const lightGrey = Color(0xFFE3E3E3);
+    static const lineHeights0 = 1.3;
+    static const lineHeights1 = 1.2;
+    static const lineHeights2 = 1.1;
+    static const lineHeights3 = 1.2;
+    static const lineHeights4 = 1.2;
+    static const lineHeights5 = 1.1;
+    static const lineHeights6 = 1.2;
+    static const lineHeights7 = 1.2;
+    static const mediumGrey = Color(0xFFB7B7B7);
+    static const mobileBodyFontFamily = 'PP Mori';
+    static const mobileBodyFontSize = 16;
+    static const mobileBodyFontWeight = 400;
+    static const mobileBodyLetterSpacing = 0;
+    static const mobileBodyLineHeight = 1.2;
+    static const mobileBodyParagraphIndent = 0;
+    static const mobileBodyParagraphSpacing = 0;
+    static const mobileBodyTextCase = null;
+    static const mobileBodyTextDecoration = null;
+    static const mobileH1FontFamily = 'PP Mori';
+    static const mobileH1FontSize = 28;
+    static const mobileH1FontWeight = 400;
+    static const mobileH1LetterSpacing = 0;
+    static const mobileH1LineHeight = 1.1;
+    static const mobileH1ParagraphIndent = 0;
+    static const mobileH1ParagraphSpacing = 0;
+    static const mobileH1TextCase = null;
+    static const mobileH1TextDecoration = null;
+    static const mobileSmallFontFamily = 'PP Mori';
+    static const mobileSmallFontSize = 12;
+    static const mobileSmallFontWeight = 400;
+    static const mobileSmallLetterSpacing = 0;
+    static const mobileSmallLineHeight = 1.2;
+    static const mobileSmallParagraphIndent = 0;
+    static const mobileSmallParagraphSpacing = 0;
+    static const mobileSmallTextCase = null;
+    static const mobileSmallTextDecoration = null;
+    static const outbid = Color(0xFFFFD2B9);
+    static const paragraphIndent0 = 0;
+    static const paragraphSpacing0 = 0;
+    static const paragraphSpacing1 = 0;
+    static const paragraphSpacing2 = 0;
+    static const paragraphSpacing3 = 0;
+    static const paragraphSpacing4 = 0;
+    static const paragraphSpacing5 = 0;
+    static const paragraphSpacing6 = 0;
+    static const paragraphSpacing7 = 0;
+    static const smallFontFamily = 'PP Mori';
+    static const smallFontSize = 16;
+    static const smallFontWeight = 400;
+    static const smallLetterSpacing = 0;
+    static const smallLineHeight = 1.2;
+    static const smallParagraphIndent = 0;
+    static const smallParagraphSpacing = 0;
+    static const smallTextCase = null;
+    static const smallTextDecoration = null;
+    static const sold = Color(0xFFE0333C);
+    static const textCaseNone = null;
+    static const textDecorationNone = null;
+    static const white = Color(0xFFFFFFFF);
+    static const winningBid = Color(0xFFBAFF99);
+}
---
+++ style-dictionary-build.mjs
@@ -0,0 +1,78 @@
+import StyleDictionary from "style-dictionary";
+import { register } from "@tokens-studio/sd-transforms";
+import { expandTypesMap } from "@tokens-studio/sd-transforms";
+
+register(StyleDictionary, {
+  excludeParentKeys: true,
+  withSDBuiltins: true,
+  "ts/color/modifiers": {
+    format: "hex",
+  },
+});
+
+StyleDictionary.registerTransform({
+  name: `dart/size/number`,
+  type: `value`,
+  transitive: true,
+  filter: (token) => {
+    return (
+      [
+        "fontSize",
+        "dimension",
+        "border",
+        "typography",
+        "shadow",
+        "letterSpacing",
+      ].includes(token.$type) &&
+      (token.$value.endsWith("px") || token.$value.endsWith("em"))
+    );
+  },
+  transform: (token) => {
+    return Number(token.$value.replace(/px|em/g, ""));
+  },
+});
+
+StyleDictionary.registerTransform({
+  name: `dart/none/null`,
+  type: `value`,
+  transitive: true,
+  filter: (token) => {
+    return token.$value === "none";
+  },
+  transform: (token) => {
+    return null;
+  },
+});
+
+const sd = new StyleDictionary({
+  source: ["design/app.tokens.json"],
+  preprocessors: ["tokens-studio"],
+  platforms: {
+    flutter: {
+      buildPath: "src/assets/design-tokens/",
+      files: [
+        {
+          destination: "tokens.dart",
+          format: "flutter/class.dart",
+          options: {
+            className: "Tokens",
+          },
+        },
+      ],
+      transformGroup: "tokens-studio",
+      transforms: [
+        "attribute/cti",
+        "color/hex8flutter",
+        "content/flutter/literal",
+        "asset/flutter/literal",
+        "dart/size/number",
+        "dart/none/null",
+      ],
+    },
+  },
+  expand: {
+    typesMap: expandTypesMap,
+  },
+});
+await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms();
