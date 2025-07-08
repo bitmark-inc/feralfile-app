90aedcbbb4d4fda5265fdab25e3bf07879a91b63
Hoang Btmrk <hoang@bitmark.com>
2025-07-08 14:59:40 +0700

    fix: remove unused code

--- lib/screen/mobile_controller/screens/index/view/channel_details/bloc/channel_detail_event.dart
+++ lib/screen/mobile_controller/screens/index/view/channel_details/bloc/channel_detail_event.dart
@@ -2,8 +2,6 @@ part of 'channel_detail_bloc.dart';
 
 abstract class ChannelDetailEvent {}
 
-class LoadChannelDetailEvent extends ChannelDetailEvent {}
-
 class LoadChannelPlaylistsEvent extends ChannelDetailEvent {
   LoadChannelPlaylistsEvent({required this.channel});
 
