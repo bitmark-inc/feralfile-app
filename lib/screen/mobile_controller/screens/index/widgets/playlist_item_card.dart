abba2e4d71ed58e7f972b0a58458982d104d114f
PuPha <nguyensanghso@gmail.com>
2025-07-17 10:28:11 +0700

    Update PlaylistCard to add Identity Bloc

--- lib/screen/mobile_controller/screens/index/widgets/playlist_item_card.dart
+++ lib/screen/mobile_controller/screens/index/widgets/playlist_item_card.dart
@@ -1,13 +1,16 @@
 import 'package:autonomy_flutter/common/injector.dart';
 import 'package:autonomy_flutter/nft_collection/models/asset_token.dart';
 import 'package:autonomy_flutter/screen/app_router.dart';
+import 'package:autonomy_flutter/screen/bloc/identity/identity_bloc.dart';
 import 'package:autonomy_flutter/screen/detail/artwork_detail_page.dart';
 import 'package:autonomy_flutter/service/navigation_service.dart';
+import 'package:autonomy_flutter/util/string_ext.dart';
 import 'package:autonomy_flutter/view/feralfile_cache_network_image.dart';
 import 'package:feralfile_app_theme/feral_file_app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class PlaylistItemCard extends StatelessWidget {
+class PlaylistItemCard extends StatefulWidget {
   const PlaylistItemCard({
     required this.asset,
     this.playlistTitle,
@@ -18,17 +21,38 @@ class PlaylistItemCard extends StatelessWidget {
   final String? playlistTitle;
 
   @override
+  State<PlaylistItemCard> createState() => _PlaylistItemCardState();
+}
+
+class _PlaylistItemCardState extends State<PlaylistItemCard> {
+  final identityBloc = injector<IdentityBloc>();
+
+  @override
+  void initState() {
+    _fetchIdentity();
+    super.initState();
+  }
+
+  void _fetchIdentity() {
+    final listIdentities = <String>[];
+    final assetToken = widget.asset;
+
+    listIdentities.addAll([assetToken.owner, assetToken.artistName ?? '']);
+    identityBloc.add(GetIdentityEvent(listIdentities));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final title = asset.title ?? '';
-    final artist = asset.artistName ?? '';
+    final title = widget.asset.title ?? '';
+    final artist = widget.asset.artistName ?? '';
     return GestureDetector(
       onTap: () {
         injector<NavigationService>().navigateTo(
           AppRouter.artworkDetailsPage,
           arguments: ArtworkDetailPayload(
-            ArtworkIdentity(asset.id, asset.owner),
+            ArtworkIdentity(widget.asset.id, widget.asset.owner),
             useIndexer: true,
-            backTitle: playlistTitle,
+            backTitle: widget.playlistTitle,
           ),
         );
       },
@@ -42,34 +66,43 @@ class PlaylistItemCard extends StatelessWidget {
             Expanded(
               child: Center(
                 child: FFCacheNetworkImage(
-                  imageUrl: asset.galleryThumbnailURL ?? '',
+                  imageUrl: widget.asset.galleryThumbnailURL ?? '',
                   fit: BoxFit.fitWidth,
                 ),
               ),
             ),
             const SizedBox(height: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  artist,
-                  style: Theme.of(context).textTheme.ppMori700White12,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  title,
-                  // italic
-                  style: Theme.of(context)
-                      .textTheme
-                      .ppMori700White12
-                      .copyWith(fontStyle: FontStyle.italic),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
+            BlocBuilder<IdentityBloc, IdentityState>(
+                bloc: identityBloc,
+                builder: (context, identityState) {
+                  final assetToken = widget.asset;
+                  final artistName = assetToken.artistName
+                          ?.toIdentityOrMask(identityState.identityMap) ??
+                      assetToken.artistID ??
+                      '';
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        artistName,
+                        style: Theme.of(context).textTheme.ppMori700White12,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        title,
+                        // italic
+                        style: Theme.of(context)
+                            .textTheme
+                            .ppMori700White12
+                            .copyWith(fontStyle: FontStyle.italic),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  );
+                }),
           ],
         ),
       ),
