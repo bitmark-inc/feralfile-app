1a6beb5db6a187d20fa065814fe18dca897fe400
Hoang Btmrk <110799121+hoangbtmrk@users.noreply.github.com>
2025-08-22 03:18:06 +0700

    Setup design tokens

---
+++ lib/design/build/components/backButton.dart
@@ -0,0 +1,23 @@
+
+//
+// /components/backButton.dart
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+
+import 'dart:ui';
+
+class BackButtonTokens {
+    BackButtonTokens._();
+
+    static const color = Color(0xFFA0A0A0);
+    static const fontSize = 12;
+    static const gap = 20;
+    static const height = 47;
+    static const iconSize = 12;
+    static const padding = 12;
+    static const radius = 90;
+    static const strokeWeight = 1;
+}
