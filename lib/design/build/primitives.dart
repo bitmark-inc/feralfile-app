77e9ec3ac000c12156fc944ccaa2996a5afca188
Hoang Btmrk <hoang@bitmark.com>
2025-08-25 16:00:30 +0700

    Build design tokens

---
+++ lib/design/build/primitives.dart
@@ -0,0 +1,39 @@
+
+//
+// primitives.dart
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+
+import 'dart:ui';
+
+class PrimitivesTokens {
+    PrimitivesTokens._();
+
+    static const colorsBlack = Color(0xFF000000);
+    static const colorsDarkGrey = Color(0xFF2E2E2E);
+    static const colorsError = Color(0xFFD10000);
+    static const colorsGrey = Color(0xFFA0A0A0);
+    static const colorsHighlight = Color(0xFFECFF0C);
+    static const colorsInactive = Color(0xFF999999);
+    static const colorsLightBlue = Color(0xFFB9E5FF);
+    static const colorsLightGrey = Color(0xFFE3E3E3);
+    static const colorsWhite = Color(0xFFFFFFFF);
+    static const fontFamiliesIbmFlexMono = "IBMPlexMono";
+    static const fontFamiliesPpMori = "PP Mori";
+    static const fontSizesBody = 14;
+    static const fontSizesH1 = 36;
+    static const fontSizesH2 = 24;
+    static const fontSizesH3 = 18;
+    static const fontSizesSmall = 12;
+    static const fontSizesTitle = 16;
+    static const fontWeightsBold = "Bold";
+    static const fontWeightsRegular = "Regular";
+    static const letterSpacings0 = 0;
+    static const paragraphIndents0 = 0;
+    static const paragraphSpacings0 = 8;
+    static const paragraphSpacings1 = 24;
+    static const spacingMainPaddingHorizontal = 12;
+}
---
+++ lib/design/build/typography.dart
@@ -0,0 +1,57 @@
+
+//
+// typography.dart
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+
+import 'dart:ui';
+
+class TypographyTokens {
+    TypographyTokens._();
+
+    static const bodyFontFamily = "PP Mori";
+    static const bodyFontSize = 14;
+    static const bodyFontWeight = "Regular";
+    static const bodyLetterSpacing = 0;
+    static const bodyParagrahpSpacing = 8;
+    static const bodyParagraphIndent = 0;
+    static const h1FontFamily = "PP Mori";
+    static const h1FontSize = 36;
+    static const h1FontWeight = "Bold";
+    static const h1LetterSpacing = 0;
+    static const h1ParagrahpSpacing = 24;
+    static const h1ParagraphIndent = 0;
+    static const h2FontFamily = "PP Mori";
+    static const h2FontSize = 24;
+    static const h2FontWeight = "Bold";
+    static const h2LetterSpacing = 0;
+    static const h2ParagrahpSpacing = 24;
+    static const h2ParagraphIndent = 0;
+    static const h3FontFamily = "PP Mori";
+    static const h3FontSize = 18;
+    static const h3FontWeight = "Bold";
+    static const h3LetterSpacing = 0;
+    static const h3ParagrahpSpacing = 24;
+    static const h3ParagraphIndent = 0;
+    static const monoFontFamily = "IBMPlexMono";
+    static const monoFontSize = 14;
+    static const monoFontWeight = "Regular";
+    static const monoLetterSpacing = 0;
+    static const monoParagrahpSpacing = 8;
+    static const monoParagraphIndent = 0;
+    static const smallFontFamily = "PP Mori";
+    static const smallFontSize = 12;
+    static const smallFontWeight = "Regular";
+    static const smallLetterSpacing = 0;
+    static const smallParagrahpSpacing = 8;
+    static const smallParagraphIndent = 0;
+    static const titleFontFamily = "PP Mori";
+    static const titleFontSize = 16;
+    static const titleFontWeight = "Regular";
+    static const titleLetterSpacing = 0;
+    static const titleParagrahpSpacing = 8;
+    static const titleParagraphIndent = 0;
+}
